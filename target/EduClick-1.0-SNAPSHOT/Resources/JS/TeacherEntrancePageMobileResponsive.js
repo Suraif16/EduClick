4088c576b127d1f5a138e53a52f40c75947e59a1
Farzan <farzanroxz123@gmail.com>
2021-09-16 18:15:47 +0530

    Completed the Teacher entrance page. Removed the system name from the header and the search button. added bttons for setting and notifications regarding requests. Adjusted the content to the right of the screen. And used media queries to adjust the page for different sized of screens. Added the like count and share count to the posts. And designed the complete notification panel. Added TeacherEntrancePageMobileResponseJS page and added funtions to show and hide notification, and also added an eventlistner on the search input element so that when the users enter the data and press enter the search function could be initiated

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/TeacherEntrancePageMobileResponsive.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/TeacherEntrancePageMobileResponsive.js
@@ -1,8 +1,18 @@
-
-// const classroomListHideButton = document.getElementById("classroomListHideButton");
-
 let classroomListStatus = false; /*if it is false the list is hidden, if it is true the list it visible*/
-const classroomListObjection = document.getElementById( "classroomsList" )
+let notificationStatus = false; /*if it is false the notification is hidden*/
+const classroomListObjection = document.getElementById( "classroomsList" );
+const search = document.getElementById( "searchBarText" );
+const notifications = document.getElementById("notifications");
+
+search.addEventListener( "keyup" , function ( event ){
+
+    if(event.key === "Enter"){
+
+        console.log(search.value);
+
+    }
+
+});
 
 function showClassroomList(){
 
@@ -15,6 +25,24 @@ function showClassroomList(){
 
         classroomListObjection.style.display = "flex";
         classroomListStatus = true;
+
+    }
+
+}
+
+function showNotification(){
+
+    console.log("hi")
+
+    if(notificationStatus){
+
+        notifications.style.display = "none";
+        notificationStatus = false;
+
+    }else{
+
+        notifications.style.display = "flex";
+        notificationStatus = true;
 
     }
 
