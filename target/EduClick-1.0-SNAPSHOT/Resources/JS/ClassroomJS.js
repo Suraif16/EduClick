fe06a1da626f94f7a158c5b9741cdc85190d1fba
Farzan <farzanroxz123@gmail.com>
2021-09-17 15:57:17 +0530

    Teacher Classroom is done here, completion of student list in the classroom (right panel). added a topic, search bar, student elements in side the student list. Each student list has a profile picture, user name, online status with the last seen details. and buttons to enable and disable the student form the classroom. And ClassroomJS file has been updated and has functions that work with the enable disable function. Teacher classroom html css and Js files were worked here

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/ClassroomJS.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/ClassroomJS.js
@@ -1,15 +1,29 @@
-const menuPanelBtn = document.getElementById("menuPanel");
-const classroomListMenu = document.getElementById("classroomsList");
 
-let statusOfClassroomListMenu = true;
+function enableDisableStatus( id ){
 
-menuPanelBtn.onclick = function (){
-    if(statusOfClassroomListMenu){
-        classroomListMenu.style.display = "none";
-        statusOfClassroomListMenu = false;
+    let enableStringValue = "enable" + id;
+
+    let disableStringValue = "disable" + id;
+
+    let enableButton = document.getElementById(enableStringValue);
+
+    let disableButton = document.getElementById(disableStringValue);
+
+    if (disableButton.style.display === "none"){
+
+        /*defaultView.getComputedStyle(enableButton)*/
+        disableButton.style.display = "block";
+        enableButton.style.display = "none";
+
+
     }else{
-        classroomListMenu.style.display = "";
-        statusOfClassroomListMenu = true;
+
+        disableButton.style.display = "none";
+        enableButton.style.display = "block";
+
     }
+
+
+
 }
 
