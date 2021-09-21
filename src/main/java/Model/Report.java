25eb190254edc148ce2b004cd4ca686d63a3e5e6
Venushka <venushkachstc@gmail.com>
2021-09-21 21:47:48 +0530

    Added class gettera and setters for admin,report and classroom

---
+++ src/main/java/Model/Report.java
@@ -0,0 +1,49 @@
+package Model;
+
+public class Report {
+    private int count;
+    private String reportFlag;
+    private String reportID;
+    private String contentID;
+    private String type;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getReportFlag() {
+        return reportFlag;
+    }
+
+    public void setReportFlag(String reportFlag) {
+        this.reportFlag = reportFlag;
+    }
+
+    public String getReportID() {
+        return reportID;
+    }
+
+    public void setReportID(String reportID) {
+        this.reportID = reportID;
+    }
+
+    public String getContentID() {
+        return contentID;
+    }
+
+    public void setContentID(String contentID) {
+        this.contentID = contentID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
