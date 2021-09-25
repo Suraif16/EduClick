9006f8142ce47e22037fdd024ac7c11642805aad
Farzan <farzanroxz123@gmail.com>
2021-09-25 14:01:32 +0530

    Added a div  to mention if the password is incorrect in html, added styles, added codes to handle incorrect passwords and incorrect password thrice

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/Login.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/Login.js
@@ -1,7 +1,10 @@
 const loginButton = document.getElementById("loginButton");
 const passwordInput = document.getElementById("password");
+const inCorrectPassword = document.getElementById("incorrectPassword");
+let passwordIncorrectCount = 0;
 
 const sendServerData = function (){
+    inCorrectPassword.style.display = "none";
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     let httpreq = new XMLHttpRequest();
@@ -29,7 +32,18 @@ const sendServerData = function (){
             // window.location.replace("/EduClick_war_exploded/Teacher/Teacher.html");
         }
         else{
-            alert("sorry");
+
+            inCorrectPassword.style.display = "flex";
+
+            passwordIncorrectCount += 1;
+
+            if (passwordIncorrectCount >= 3){
+
+                // window.location.replace("/EduClick_war_exploded/Teacher/Teacher.html");
+                /*here if the password is incorrect the user will be sent to a different page to handle it*/
+            }
+            console.log( passwordIncorrectCount );
+
         }
 
     }
