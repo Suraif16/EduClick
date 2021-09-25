967100e9ab65992a6cb91acf89f4ab8210883b2e
Venushka <venushkachstc@gmail.com>
2021-09-25 11:12:44 +0530

    Modified registration page

--- src/main/java/Controller/RegisterServlet.java
+++ src/main/java/Controller/RegisterServlet.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.LocalTime;
-//import org.json.JSONObject;
+import org.json.JSONObject;
 
 public class RegisterServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -20,7 +20,7 @@ public class RegisterServlet {
         response.setContentType("text/html");
 
         HttpSession session = request.getSession();
-        //JSONObject jsonObject = new JSONObject();
+        JSONObject jsonObject = new JSONObject();
 
         String firstname = request.getParameter("Firstname");
         String lastname = request.getParameter("Lastname");
@@ -31,6 +31,8 @@ public class RegisterServlet {
         LocalDate registrationDate = LocalDate.now();
         LocalTime registrationTime = LocalTime.now();
         String gender = request.getParameter("Gender");
+
+        System.out.println(firstname);
 
         User user = new User();
 
@@ -44,15 +46,9 @@ public class RegisterServlet {
         user.setRegistrationTime(registrationTime);
         user.setGender(gender);
 
-        String userRegistered = RegisterDAO.registerUser(user);
 
-        if(userRegistered.equals("SUCCESS")){
-            request.getRequestDispatcher("/Home.html").forward(request,response);
-        }
-        else{
-            request.setAttribute("errMessage",userRegistered);
-            request.getRequestDispatcher("/RegisterNew.html").forward(request,response);
-        }
+
+
 
     }
 }
