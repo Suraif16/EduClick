21a3c04c7e3391edba2038f6a6711320f9fd219d
Venushka <venushkachstc@gmail.com>
2021-09-25 17:51:23 +0530

    Modified registration page html ,css and js

--- src/main/webapp/Resources/JS/Registration.js
+++ src/main/webapp/Resources/JS/Registration.js
@@ -1,54 +1,74 @@
-const submitButton = document.getElementById("submitButton");
+const submitButton = document.getElementById("button");
+console.log("Haaaaa!");
 
-const sendServerData = function (){
+const sendServerData = function () {
+
+    console.log("Hahahahahaha");
 
     let firstName = document.getElementById("firstName").value;
     let lastName = document.getElementById("lastName").value;
     let email = document.getElementById("email").value;
-    let dateOfBirth = document.getElementById("dateOfBirth").value;
-    let userType = document.getElementById("userType").value;
+    let dateOfBirth = document.getElementById("DOB").value;
+    let userTypeTeacher = document.getElementById("teacher").value;
+    let userTypeStudent = document.getElementById("student").value;
     let country = document.getElementById("country").value;
     let city = document.getElementById("city").value;
-    let mobileNumber = document.getElementById("mobileNumber").value;
-    let gender = document.getElementById("gender").value;
+    let mobileNumber = document.getElementById("mobileno").value;
+    let genderMale = document.getElementById("male").value;
+    let genderFemale = document.getElementById("female").value;
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirmPassword").value;
 
-    let httpreq = new XMLHttpRequest();
-    httpreq.onreadystatechange = function (){
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+    console.log(dateOfBirth);
+    console.log(userTypeTeacher);
+    console.log(userTypeStudent);
+    console.log(country);
+    console.log(city);
+    console.log(mobileNumber);
+    console.log(genderMale);
+    console.log(genderFeale);
+    console.log(password);
+    console.log(confirmPassword);
 
-        if (this.readyState === 4 && this.status === 200){
-            completeRegistration( this ) /*This is where we get the response when the request was successfully sent and a successfully response is received */
+    /*let httpReq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function () {
+
+        if (this.readyState === 4 && this.status === 200) {
+            completeRegistration(this) /!*This is where we get the response when the request was successfully sent and a successfully response is received *!/
         }
 
     }
 
-    httpreq.open( "POST" , "/EduClick_war_exploded/Registration" , true);
-    httpreq.setRequestHeader("Content-type" , "application/x-www-form-urlencoded");
-    httpreq.send("firstName=" + firstName + "&lastName=" + lastName + "&email=" + email + "&dateOfBirth=" + dateOfBirth + "&userType=" + userType + "&country=" + country + "&city=" + city + "&mobileNumber=" + mobileNumber + "&gender=" + gender + "&Password=" + password + "&confirmPassword=" + confirmPassword );
+    httpreq.open("POST", "/EduClick_war_exploded/Registration", true);
+    httpreq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    httpreq.send("firstName=" + firstName + "&lastName=" + lastName + "&email=" + email + "&dateOfBirth=" + dateOfBirth + "&userType=" + userType + "&country=" + country + "&city=" + city + "&mobileNumber=" + mobileNumber + "&gender=" + gender + "&Password=" + password + "&confirmPassword=" + confirmPassword);
+*/
+    /*function completeRegistration(httpreq) {
 
-    function completeRegistration( httpreq ){
-
-        let jsonRegistrationResponse = JSON.parse(httpreq.responseText); /*here when we receive the response
+        let jsonRegistrationResponse = JSON.parse(httpreq.responseText); /!*here when we receive the response
         from the server, we convert it to JSON as it will be sent as JSON from the servlet.
         Once we parse the response to JSON we use jsonLoginResponse.User to get the value of User member
-        in the JSON object specified by the servlet*/
-        console.register(jsonRegistrationResponse.User);
-        if ( jsonRegistrationResponse.User === "Teacher"){
-            console.register("in if");
+        in the JSON object specified by the servlet*!/
+        console.log(jsonRegistrationResponse.User);
+        if (jsonRegistrationResponse.User === "Teacher") {
+            console.log("in if");
             // window.location.replace("/EduClick_war_exploded/Teacher/Teacher.html");
-        }
-        else{
-            ( jsonRegistrationResponse.User === "Student"){
-                console.register("in if");
+        } else {
+            (jsonRegistrationResponse.User === "Student")
+            {
+                console.log("in if");
                 // window.location.replace("/EduClick_war_exploded/Student/Student.html");
 
 
+            }
+
         }
 
-    }
 
-
+    }*/
 }
 
 submitButton.onclick = function (){
