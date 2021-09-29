8ff459e1799b053c5e9711da6f7ddfd7e278f622
Farzan <farzanroxz123@gmail.com>
2021-09-29 17:45:16 +0530

    added a function in the teacherentrancepageMObileResponsive.js to show the hidded addclassroomform, and adjusted css and html accrodingly

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/TeacherEntrancePageMobileResponsive.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/TeacherEntrancePageMobileResponsive.js
@@ -1,9 +1,11 @@
 let classroomListStatus = false; /*if it is false the list is hidden, if it is true the list it visible*/
 let notificationStatus = false; /*if it is false the notification is hidden*/
+let addClassroomFormStatus = false /*if it is false the addClassroomForm is hidden*/
 
 const classroomListObjection = document.getElementById( "classroomsList" );
 const search = document.getElementById( "searchBarText" );
 const notifications = document.getElementById("notifications");
+const addClassroomForm = document.getElementById("addClassroomForm");
 
 search.addEventListener( "keyup" , function ( event ){
 
@@ -45,6 +47,22 @@ function showNotification(){
 
         notifications.style.display = "flex";
         notificationStatus = true;
+
+    }
+
+}
+
+function showAddClassroomFrom(){
+
+    if(addClassroomFormStatus){
+
+        addClassroomForm.style.display = "none";
+        addClassroomFormStatus = false;
+
+    }else {
+
+        addClassroomForm.style.display = "flex";
+        addClassroomFormStatus = true;
 
     }
 
