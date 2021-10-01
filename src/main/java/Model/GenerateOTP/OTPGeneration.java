656af01b4bb8e2b3edd258b881c44328bb68adba
Farzan <farzanroxz123@gmail.com>
2021-10-01 13:28:15 +0530

    created a package for OPTGeneration, and also create a java file/component that generates an OTP

---
+++ src/main/java/Model/GenerateOTP/OTPGeneration.java
@@ -0,0 +1,36 @@
+package Model.GenerateOTP;
+
+import java.util.Random;
+
+public class OTPGeneration {
+
+
+    public String generateOTP( int length ){
+
+        /* In java String is immutable white StringBuilder is mutable
+        * therefore to build we use StringBuilder */
+
+        StringBuilder OTP = new StringBuilder();
+
+        String capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String simpleLetters = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+        String symbols = "!@#$%^&*_-=+/.?<>";
+
+        String possibleCharacters = capitalLetters + simpleLetters + numbers + symbols;
+
+        Random random = new Random();
+
+        for ( int i = 0; i < length ; i++){
+
+            /* Here we select a random number between zero (inclusive) and length of possibleCharacters (exclusive)
+            * , then using this random number we select a character at than index in possibleCharacters
+            * and we append this character to the OTP */
+            OTP.append( possibleCharacters.charAt( random.nextInt( possibleCharacters.length() ) ) );
+
+        }
+
+        return OTP.toString();
+    }
+
+}
