9a202b1a4e05811a87f3bb041ca31e34f80be5a5
Farzan <farzanroxz123@gmail.com>
2021-10-07 21:18:57 +0530

    added Logout Servlet file

---
+++ src/main/java/Controller/LogoutServlet.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest httpServletRequest , HttpServletResponse httpServletResponse){
+
+        
+
+    }
+
+}
