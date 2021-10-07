cda02230578c647296fe4ce3dff9c2d9f4e576e4
Farzan <farzanroxz123@gmail.com>
2021-10-07 23:25:00 +0530

    Deleted TeacherLoadClassroomList.js file and transfered it's codes into TeacherLoad.js file. And adjusted the Teacher.html file accordingly

--- src/main/webapp/Resources/JS/TeacherLoad.js
+++ src/main/webapp/Resources/JS/TeacherLoad.js
@@ -4,14 +4,16 @@ document.onreadystatechange = function (){
 
     if ( document.readyState === 'complete' ){
         /* when the document is loaded and complete this function will run*/
+        console.log("teacherload");
         sendServerData();
+        getClassroomList();
 
     }
 
 }
 
 const sendServerData = function (){
-
+    /* This function gets the username from the server*/
     let httpreq = new XMLHttpRequest();
     httpreq.onreadystatechange = function (){
 
@@ -34,6 +36,50 @@ const sendServerData = function (){
             /* This is where I need work everytime as per the authentication filter*/
             const name = document.getElementById("headerUserName");
             name.innerHTML = jsonLoginResponse.firstName;
+        }else{
+            alert("something went wrong!!!");
+        }
+
+    }
+
+
+}
+
+const getClassroomList = function (){
+    /* This function gets the Lists of classrooms from the server*/
+
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+
+        if (this.readyState === 4 && this.status === 200){
+            complete( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherLoadClassroomList" , true);
+    httpreq.send();
+
+    function complete( httpreq ){
+
+        let jsonResponse = JSON.parse(httpreq.responseText);
+
+        if( jsonResponse.serverResponse === "null Session" || jsonResponse.serverResponse === "Not Allowed"){
+            window.location.replace("/EduClick_war_exploded/Login.html");
+        }else if(jsonResponse.serverResponse === "Allowed") {
+            /* This is where I need work everytime as per the authentication filter*/
+
+            console.log(jsonResponse.UserObject);
+            console.log(jsonResponse.UserObject.city);
+            console.log(jsonResponse.ArrayValues);
+            console.log(jsonResponse.ArrayValues.length)
+            let count = jsonResponse.ArrayValues.length - 1;
+            while( count > 0 ){
+
+                console.log(jsonResponse.ArrayValues[count]);
+                count --;
+            }
+
         }else{
             alert("something went wrong!!!");
         }
