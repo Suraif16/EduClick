3f3445574c38d1e920e4772fb7e5b2fd7802c6cb
Farzan <farzanroxz123@gmail.com>
2021-10-07 21:46:14 +0530

    completed the logoutServlet and added servlet mapping to web.xml file. And adjusted the Teacher.html and Classroom.html file to have logout functions

--- src/main/java/Controller/LogoutServlet.java
+++ src/main/java/Controller/LogoutServlet.java
@@ -3,13 +3,18 @@ package Controller;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
 
     @Override
-    public void doGet(HttpServletRequest httpServletRequest , HttpServletResponse httpServletResponse){
+    public void doGet(HttpServletRequest httpServletRequest , HttpServletResponse httpServletResponse) throws IOException {
 
-        
+        HttpSession session = httpServletRequest.getSession();
+        session.invalidate();
+
+        httpServletResponse.sendRedirect("Login.html");
 
     }
 
