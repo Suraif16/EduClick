fee89145febbca336649b9b60dc3cd81c75b89e5
Farzan <farzanroxz123@gmail.com>
2021-10-07 20:31:26 +0530

    server restart

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/Admin.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/Admin.js
@@ -1,25 +1,38 @@
 //document.getElementById("demo").innerHTML = 5;
 
-let httpreq = new XMLHttpRequest();
-httpReq.onreadystatechange = function () {
+document.onreadystatechange = function (){
+    if ( document.readyState === 'complete' ){
+        /* when the document is loaded and complete this function will run*/
+        console.log("page loaded");
+        sendServerData();
+        console.log("page complete");
+    }
+}
+const sendServerData = function (){
 
-    if (this.readyState === 4 && this.status === 200) {
-        completecount(this) /*This is where we get the response when the request was successfully sent and a successfully response is received */
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+
+        if (this.readyState === 4 && this.status === 200) {
+            completecount(this) /*This is where we get the response when the request was successfully sent and a successfully response is received */
+        }
+
+    }
+    httpReq.open( "GET" , "/EduClick_war_exploded/Admin" , true);
+    httpReq.send();
+
+
+    function completecount( httpreq ) {
+        let jsonLoginResponse = JSON.parse(httpreq.responseText);
+        console.log(jsonLoginResponse.Teacher);
+        //document.getElementById("demo").innerHTML = jsonLoginResponse.Teacher;
+
+        let count = jsonLoginResponse.Teacher;
+        document.getElementById("demo").innerHTML = count;
     }
 
 }
-httpReq.open("GET", "/EduClick_war_exploded/Admin", true);
 
-
-function completecount(httpReq) {
-    let jsonLoginResponse = JSON.parse(httpreq.responseText);
-    console.log(jsonLoginResponse.Teacher);
-document.getElementById("demo").innerHTML = jsonLoginResponse.Teacher;
-
-let count = jsonLoginResponse.Teacher;
-
-//document.getElementById("demo").innerHTML = count;
-}
 
 
 
