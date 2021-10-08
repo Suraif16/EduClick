9e4f47fe069999ac55a07cb77cee7337c7ac3ce9
Rahuram <rahuram66@gmail.com>
2021-10-08 22:33:32 +0530

    Editing Admin Servlet for get count

--- src/main/java/Controller/AdminServlet.java
+++ src/main/java/Controller/AdminServlet.java
@@ -1,6 +1,8 @@
 package Controller;
 
 import DAO.UserDAO;
+import Model.Admin;
+import Model.User;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServlet;
@@ -27,6 +29,11 @@ public class AdminServlet extends HttpServlet {
         UserDAO userDAO = new UserDAO();
         int count= userDAO.countTeacher();
         jsonObject.put("Teacher" ,count );
+        Admin admin = new Admin();
+        int count1= admin.getcountteacher();
+        int count2= admin.gettodaycountteacher();
+        int count3= admin.getcountstudent();
+        int count4= admin.gettodaycountstudent();
 
         System.out.println(jsonObject);
         out.write(jsonObject.toString());
