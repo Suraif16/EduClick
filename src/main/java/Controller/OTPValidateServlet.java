6d20ffc9c0b1c808508d6728edb473c1b7bc5697
Farzan <farzanroxz123@gmail.com>
2021-10-13 04:05:20 +0530

    completed the OTPValidateSErvlet.java file, when the user submits the otp code, this servlet verifies it and send the response to the js file

--- src/main/java/Controller/OTPValidateServlet.java
+++ src/main/java/Controller/OTPValidateServlet.java
@@ -1,15 +1,55 @@
 package Controller;
 
+import Model.User;
+import org.json.JSONObject;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class OTPValidateServlet extends HttpServlet {
 
     @Override
-    public void doPost(HttpServletRequest request , HttpServletResponse response ){
+    public void doPost(HttpServletRequest request , HttpServletResponse response ) throws IOException {
 
-        System.out.println("optValidate");
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+        JSONObject jsonObject = new JSONObject();
+
+        String OPTUserValue = request.getParameter( "OPTUserValue" );
+
+        HttpSession session = request.getSession( false );
+
+        String OTPValue = (String) session.getAttribute( "OPT Code" );
+
+        User user = (User) session.getAttribute( "User" );
+
+        if(OPTUserValue.equals(OTPValue)){
+
+            jsonObject.put( "OTPStatus" , "valid" );
+
+            if( user.getUserType().equals("Teacher") ){
+
+                jsonObject.put("UserType" , "Teacher");
+
+            }else if (user.getUserType().equals("Student")){
+
+                jsonObject.put("UserType" , "Student");
+
+            }
+
+
+        }else{
+
+            jsonObject.put( "OTPStatus" , "invalid" );
+
+        }
+
+        out.write(jsonObject.toString());
+        out.close();
 
     }
 
