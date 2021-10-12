6926e1dc64f255f587636ccd937d29b9c2f8be19
Venushka <venushkachstc@gmail.com>
2021-10-12 13:34:05 +0530

    Added the filter mappings to xml file

---
+++ src/main/java/Controller/RegisterFilter.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class RegisterFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Register filter reached!!!!!!");
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
