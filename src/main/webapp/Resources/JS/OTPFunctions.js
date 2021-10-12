9e777533fa21c84d03f0cda39cf17670350f3ab8
Farzan <farzanroxz123@gmail.com>
2021-10-13 00:41:18 +0530

    added few comments

--- src/main/webapp/Resources/JS/OTPFunctions.js
+++ src/main/webapp/Resources/JS/OTPFunctions.js
@@ -1,27 +1,27 @@
 const verifyOTPButton = document.getElementById("verifyOTP");
 const resendOTPButton = document.getElementById("resendOTP");
 document.onreadystatechange = function (){
-
+    /* runs when the DOM is laoded */
     if (document.readyState === 'complete'){
         generateOTP();
     }
 }
 
 const generateOTP = function (){
-
+    /* calls a servlet to generate an OTP */
     console.log("OTP GENERATED");
 
 }
 
 verifyOTPButton.onclick = function (){
-
+    /* again send user input to server and checks otp correction */
     const otpValue = document.getElementById("otp").value;
     console.log("value of otp" , otpValue);
 
 }
 
 resendOTPButton.onclick = function (){
-
+    /* to regenerate a new OTP */
     generateOTP();
 
 }
