90885fab863de74097a6a66672368ca13e3deda7
Farzan <farzanroxz123@gmail.com>
2021-10-12 21:27:52 +0530

    server restart

---
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/ClassroomJS.js
@@ -0,0 +1,83 @@
+let rightPanelStatus = false; /*if it is false the list is hidden, if it is true the list it visible*/
+const rightPanel = document.getElementById("rightPanel");
+
+function showRightPanel(){
+
+    if(rightPanelStatus){
+
+        rightPanel.style.display = "none";
+        rightPanelStatus = false;
+
+    }else{
+
+        rightPanel.style.display = "flex";
+        rightPanelStatus = true;
+
+    }
+
+}
+
+
+function enableDisableStatus( id ){
+
+    let enableStringValue = "enable" + id;
+
+    let disableStringValue = "disable" + id;
+
+    let enableButton = document.getElementById(enableStringValue);
+
+    let disableButton = document.getElementById(disableStringValue);
+
+    if (disableButton.style.display === "none"){
+
+        /*defaultView.getComputedStyle(enableButton)*/
+        disableButton.style.display = "block";
+        enableButton.style.display = "none";
+
+
+    }else{
+
+        disableButton.style.display = "none";
+        enableButton.style.display = "block";
+
+    }
+
+}
+
+function showAnswers( id ){
+
+    let answerId = "answersInPost" + id;
+    let answerContainer = document.getElementById( answerId );
+
+    if (answerContainer.style.display === "none"){
+
+        answerContainer.style.display = "flex";
+
+    }else{
+
+        answerContainer.style.display = "none";
+
+    }
+
+
+}
+
+
+function showMcqResult( id ){
+
+    let mcqResultsInPostId = "mcqResultsInPost" + id;
+    let mcqResultsInPost = document.getElementById( mcqResultsInPostId );
+
+    if (mcqResultsInPost.style.display === "none"){
+
+        mcqResultsInPost.style.display = "flex";
+
+    }else{
+
+        mcqResultsInPost.style.display = "none";
+
+    }
+
+
+}
+
---
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/StudentEntrancePageMobileResponsive.js
@@ -0,0 +1,108 @@
+let classroomListStatus = false; /*if it is false the list is hidden, if it is true the list it visible*/
+let notificationStatus = false; /*if it is false the notification is hidden*/
+
+const classroomListObjection = document.getElementById( "classroomsList" );
+const search = document.getElementById( "searchBarText" );
+const notifications = document.getElementById("notifications");
+
+search.addEventListener( "keyup" , function ( event ){
+
+    if(event.key === "Enter"){
+
+        console.log(search.value);
+
+    }
+
+});
+
+function showClassroomList(){
+
+    if(classroomListStatus){
+
+        classroomListObjection.style.display = "none";
+        classroomListStatus = false;
+
+    }else{
+
+        classroomListObjection.style.display = "flex";
+        classroomListStatus = true;
+
+    }
+
+}
+
+function showNotification(){
+
+    console.log("hi")
+
+    if(notificationStatus){
+
+        notifications.style.display = "none";
+        notificationStatus = false;
+
+    }else{
+
+        notifications.style.display = "flex";
+        notificationStatus = true;
+
+}
+
+}
+/*
+document.onreadystatechange = function (){
+
+    if ( document.readyState === 'complete' ){
+        /!* when the document is loaded and complete this function will run*!/
+        console.log("page loaded");
+        sendServerData();
+        console.log("page complete");
+
+    }
+
+}
+const sendServerData = function (){
+
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+
+        if (this.readyState === 4 && this.status === 200){
+            completeLogin( this ); /!*This is where we get the response when the request was successfully sent and a successfully response is received *!/
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/student/studentNewsFeedLoaded" , true);
+    httpreq.send();
+
+    function completeLogin( httpreq ){
+
+
+        let jsonResponse = JSON.parse(httpreq.responseText);
+        let arr = new Array();
+        arr = jsonResponse;
+
+
+        /!*if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
+            window.location.replace("/EduClick_war_exploded/Login.html");
+        } if(jsonLoginResponse.serverResponse === "Allowed") {
+            /!* This is where I need work everytime as per the authentication filter*!/
+            const name = document.getElementById("headerUserName");
+            name.innerHTML = jsonLoginResponse.firstName;
+            console.log( jsonLoginResponse.firstName , jsonLoginResponse.lastName);
+        }else{
+            alert("something went wrong!!!");
+        }*!/
+
+
+            /!*console.log(arr[0].values().value())*!/
+
+
+
+
+
+    }
+
+
+
+
+}*/
