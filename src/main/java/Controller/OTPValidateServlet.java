8f1b360495aa17027a57815528854da1b72c3b50
Farzan <farzanroxz123@gmail.com>
2021-10-13 00:57:58 +0530

    created OTPGenerateServlet and OTPValidateServlet and servlet mapping are given in the web.xml file

---
+++ src/main/java/Controller/OTPValidateServlet.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OTPValidateServlet extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest request , HttpServletResponse response ){
+
+
+
+    }
+
+}
