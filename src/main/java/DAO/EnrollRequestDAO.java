a5cd96e467c5a84931fbb73c1a183a2d1f9f0479
Farzan <farzanroxz123@gmail.com>
2021-10-14 22:58:08 +0530

    completed the servlet to select all requests from EnrollRequestDAO and FriendRequestDAO, in which added functions to query the database. Created Request.java class to handle request data

--- src/main/java/DAO/EnrollRequestDAO.java
+++ src/main/java/DAO/EnrollRequestDAO.java
@@ -1,10 +1,15 @@
 package DAO;
 
 import Database.DBConnectionPool;
+import Model.Classroom;
+import Model.Requests;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EnrollRequestDAO {
     public void insertRecord(String classrooomId,String userId){
@@ -48,6 +53,45 @@ public class EnrollRequestDAO {
             preparedStatement.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
         }
+
+    }
+
+    public ArrayList< Requests > selectAll( Classroom classroom ){
+
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+
+        ArrayList< Requests > requestsList = new ArrayList<>();
+
+        try{
+
+            connection = dbConnectionPool.dataSource.getConnection();
+            String sql = "SELECT From_UserID FROM Enroll_Request WHERE To_ClassroomID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            preparedStatement.setString( 1 , classroom.getClassroomID() );
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while ( resultSet.next() ){
+
+                String fromId = resultSet.getString( "From_UserID" );
+                String classroomDescription = "wants to join to " + classroom.getClassroomName() +
+                        " : " + classroom.getSubject() + " : grade " + classroom.getGrade() + " : " +
+                        classroom.getYear();
+                Requests requests = new Requests( fromId , classroom.getClassroomID() , "Enroll" , classroomDescription );
+                requestsList.add( requests );
+            }
+
+        }catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        finally {
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
+        }
+
+        return requestsList;
+
     }
 }
--- src/main/java/Model/Requests.java
+++ src/main/java/Model/Requests.java
@@ -1,23 +1,38 @@
 package Model;
 
+import DAO.EnrollRequestDAO;
+import DAO.FriendRequestDAO;
+
+import java.util.ArrayList;
+
 public class Requests {
 
     private String fromId;
     private String toId;
-    private String type;
+    private String type; /* Enroll , Friend */
+    private String description;
+    private String userName;
+    private String userProfile;
 
     /* Here fromId is always a userId , toId will depend on the type of the request,
     * if the request is a friend request they toId is a userId , else if the request is a
     * enroll request type then toId is a ClassroomId, Since we use Bigint for both userId and
     * ClassroomId it doesn't change anything.
     * Here fromId is the userId of the user who sends the request, and toId is to a user who receives
-    * the request, or to a classroom which receives the request*/
+    * the request, or to a classroom which receives the request
+    * Here the Description will will be the classroom details if the type is enroll request
+    * else it would say wants to be your friend*/
 
-    public Requests( String fromId, String toId, String type ) {
+    public Requests( String fromId, String toId, String type , String description) {
 
         this.fromId = fromId;
         this.toId = toId;
         this.type = type;
+        this.description = description;
+
+    }
+
+    public Requests(){
 
     }
 
@@ -44,4 +59,47 @@ public class Requests {
     public void setType(String type) {
         this.type = type;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserProfile() {
+        return userProfile;
+    }
+
+    public void setUserProfile(String userProfile) {
+        this.userProfile = userProfile;
+    }
+
+    public ArrayList< Requests > selectRequests(Classroom classroom ){
+
+            EnrollRequestDAO enrollRequestDAO = new EnrollRequestDAO();
+            return enrollRequestDAO.selectAll( classroom );
+
+
+    }
+
+    public ArrayList< Requests > selectRequests( String userId ){
+
+        FriendRequestDAO friendRequestDAO = new FriendRequestDAO();
+        return friendRequestDAO.selectAll( userId );
+
+
+    }
+
+
+
 }
