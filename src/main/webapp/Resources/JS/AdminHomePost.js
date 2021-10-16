9d636d15280f5c1e721d9f888ece0c7d90c6e2c1
Rahuram <rahuram66@gmail.com>
2021-10-16 23:23:23 +0530

    Adding functions

--- src/main/webapp/Resources/JS/AdminHomePost.js
+++ src/main/webapp/Resources/JS/AdminHomePost.js
@@ -3,79 +3,50 @@ let addClassroomFormStatus = false; /*if it is false the addClassroomForm is hid
 let addClassroomFormErrorStatus = false; /* if it is false then classroomFormRowErrorMessage is hidden*/
 
 const classroomListObjection = document.getElementById( "classroomsList" );
-const search = document.getElementById( "searchBarText" );
 const addClassroomForm = document.getElementById("addClassroomForm");
 const classroomFormRowErrorMessage = document.getElementById( "classroomFormRowErrorMessage" );
+const submitButton = document.getElementById("postButton");
 
-search.addEventListener( "keyup" , function ( event ){
 
-    if(event.key === "Enter"){
 
-        console.log(search.value);
-
-    }
-
-});
 
 function showClassroomList(){
-
     if(classroomListStatus){
-
         classroomListObjection.style.display = "none";
         classroomListStatus = false;
-
     }else{
-
         classroomListObjection.style.display = "flex";
         classroomListStatus = true;
-
     }
-
 }
 
 function showAddClassroomFrom(){
-
     if(addClassroomFormStatus){
-
         addClassroomForm.style.display = "none";
         addClassroomFormStatus = false;
-
     }else {
-
         addClassroomForm.style.display = "flex";
         addClassroomFormStatus = true;
-
     }
-
 }
 
 function showClassroomFormRowErrorMessage( message ){
-
     if(addClassroomFormErrorStatus){
-
         classroomFormRowErrorMessage.style.display = "none";
         addClassroomFormErrorStatus = false;
-
     }else {
-
         classroomFormRowErrorMessage.style.display = "flex";
         classroomFormRowErrorMessage.innerHTML = message;
         addClassroomFormErrorStatus = true;
-
     }
-
 }
 
 function createClassroom(){
-
     const classroomsListLinks = document.getElementById("classroomsListLinks");
     let classroomName = document.getElementById("classroomName");
     let yearOfExamination = document.getElementById("classroomYearOfExamination");
     let gradeClass = document.getElementById("classroomClassGrade");
     let subject = document.getElementById("classroomSubject");
-
-
-
 
     const createClassroomHtml = function ( classroomId ){
 
@@ -126,73 +97,25 @@ function createClassroom(){
                 /* This is where I need work everytime as per the authentication filter*/
 
                 if( jsonResponse.classroomId == "Classroom not created" ){
-
                     showClassroomFormRowErrorMessage( "Classroom creation was unsuccessful. Please try again later..." );
-
                 }else {
-
                     createClassroomHtml( jsonResponse.classroomId );
                     showClassroomFormRowErrorMessage();
-
                 }
-
-
             }else{
                 alert("something went wrong!!!");
             }
-
         }
-
-
     }
 
 
     if( classroomName.value === "" || yearOfExamination.value === "" || gradeClass.value === "" || subject.value === "" ){
-
         showClassroomFormRowErrorMessage( "Please fill all input fields..." );
 
-
     }else {
-
         sendServerData();
-
     }
-
 
 }
 
-/* Search for Teachers start here  */
-search.addEventListener( "keyup" , function (event){
 
-    const searchName = document.getElementById("searchBarText").value;
-
-    console.log(searchName);
-    searchTeacher();
-
-});
-
-function searchTeacher(){
-
-    const searchName = document.getElementById("searchBarText").value;
-
-    let httpreq = new XMLHttpRequest();
-    httpreq.onreadystatechange = function (){
-
-        if (this.readyState === 4 && this.status === 200){
-            completeSearch( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
-        }
-    }
-    /* ************************ */
-    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherCreateClassroom" , true);
-    httpreq.setRequestHeader("Content-type" , "application/x-www-form-urlencoded");
-    httpreq.send("searchBarText=" + searchName);
-
-    function completeSearch(httpreq){
-
-        console.log(httpreq.responseText);
-        let jsonSearchResponse = JSON.parse(httpreq.responseText);
-
-    }
-
-
-}
