1d2a6c452eac949c8436b6fb350df3e0c6ef0fe7
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-10-16 11:50:31 +0530

    change servlert file

--- src/main/java/Controller/SearchTeacherServlet.java
+++ src/main/java/Controller/SearchTeacherServlet.java
@@ -2,22 +2,32 @@ package Controller;
 
 import org.json.JSONObject;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class SearchTeacherServlet extends HttpServlet {
 
 
-    protected void goGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
         JSONObject jsonObject = new JSONObject();
-        String firstname = request.getParameter("searchBarValue");
-        System.out.println(firstname);
+        jsonObject.put("serverResponse" , "Allowed");
+
+        HttpSession session = request.getSession();
+
+        String teacherName = request.getParameter("searchBarValue");
+
+        System.out.println(teacherName);
+
+        out.write(jsonObject.toString());
+        out.close();
     }
 
 }
