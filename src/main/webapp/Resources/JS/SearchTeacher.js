e588063e5227104d1773d49ef01aeeae925dd443
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-10-16 11:51:45 +0530

    add js file

--- src/main/webapp/Resources/JS/SearchTeacher.js
+++ src/main/webapp/Resources/JS/SearchTeacher.js
@@ -8,29 +8,42 @@ searchValue.addEventListener( "keyup" , function (event){
 
 });
 
-function searchForTeacher(){
+function searchForTeacher() {
 
     const searchBarValue = document.getElementById("searchBarText").value;
+    console.log(searchBarValue);
 
     let httpreq = new XMLHttpRequest();
     httpreq.onreadystatechange = function (){
 
         if (this.readyState === 4 && this.status === 200){
-            completeSearch( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+            console.log("hh");
+            completeSearch( this );
         }
     }
-    /* ************************ */
-    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacher" , true);
+    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/searchTeacher" , true);
     httpreq.setRequestHeader("Content-type" , "application/x-www-form-urlencoded");
     httpreq.send("searchBarText=" + searchBarValue);
-
-    function completeSearch(httpreq){
-
-        console.log(httpreq.responseText);
-        let jsonSearchResponse = JSON.parse(httpreq.responseText);
-
-    }
-
-
+    console.log("bbb");
 
 }
+const completeSearch = function( httpreq ){
+
+
+    let jsonResponse = JSON.parse( httpreq.responseText);
+
+    if( jsonResponse.serverResponse === "null Session" || jsonResponse.serverResponse === "Not Allowed"){
+        window.location.replace("/EduClick_war_exploded/Login.html");
+    }else if(jsonResponse.serverResponse === "Allowed") {
+        /* This is where I need work everytime as per the authentication filter*/
+
+        displayRequest( jsonResponse );
+
+    }else{
+        alert("something went wrong!!!");
+    }
+
+    
+
+}
+
