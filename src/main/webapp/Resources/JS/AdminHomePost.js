218873547c340a9b4341816001c651d933357482
Rahuram <rahuram66@gmail.com>
2021-10-17 18:19:50 +0530

    Finishing insert function

--- src/main/webapp/Resources/JS/AdminHomePost.js
+++ src/main/webapp/Resources/JS/AdminHomePost.js
@@ -7,9 +7,6 @@ const addClassroomForm = document.getElementById("addClassroomForm");
 const classroomFormRowErrorMessage = document.getElementById( "classroomFormRowErrorMessage" );
 const submitButton = document.getElementById("postButton");
 
-
-
-
 function showClassroomList(){
     if(classroomListStatus){
         classroomListObjection.style.display = "none";
@@ -41,81 +38,42 @@ function showClassroomFormRowErrorMessage( message ){
     }
 }
 
-function createClassroom(){
-    const classroomsListLinks = document.getElementById("classroomsListLinks");
-    let classroomName = document.getElementById("classroomName");
-    let yearOfExamination = document.getElementById("classroomYearOfExamination");
-    let gradeClass = document.getElementById("classroomClassGrade");
-    let subject = document.getElementById("classroomSubject");
-
-    const createClassroomHtml = function ( classroomId ){
 
 
-        classroomsListLinks.innerHTML += '<div className="classroomsListLinksItems"' +
-            ' style="flex: 1;\n' +
-            '    background-color: #4775c4;\n' +
-            '    text-align: center;\n' +
-            '    margin: 1.5% 0;\n' +
-            '    padding: 1%;"> ' +
-            '<a href="/EduClick_war_exploded/Teacher/Classroom.html?id=' + classroomId +'"' +' className="classRooms"> ' +
-            classroomName.value + ' : ' + subject.value + ' : Grade ' + gradeClass.value + ' : ' + yearOfExamination.value +
-            '</a>' +
-            '</div>';
+const sendServerData = function (){
+    let textMsg = document.getElementById("classroomName").value;
 
 
-        classroomName.value = "";
-        yearOfExamination.value = "";
-        gradeClass.value = "";
-        subject.value = "";
-        console.log(classroomId);
-        showAddClassroomFrom();
-
-    }
-
-    const sendServerData = function (){
-
-        let httpreq = new XMLHttpRequest();
-        httpreq.onreadystatechange = function (){
-
-            if (this.readyState === 4 && this.status === 200){
-                completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
-            }
-
-        }
-
-        httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherCreateClassroom" , true);
-        httpreq.setRequestHeader("Content-type" , "application/x-www-form-urlencoded");
-        httpreq.send("classroomName=" + classroomName.value + "&subject=" + subject.value + "&grade=" + gradeClass.value + "&yearOfExamination=" + yearOfExamination.value );
-
-        function completeLogin( httpreq ){
-
-            let jsonResponse = JSON.parse(httpreq.responseText);
-
-            if( jsonResponse.serverResponse === "null Session" || jsonResponse.serverResponse === "Not Allowed"){
-                window.location.replace("/EduClick_war_exploded/Login.html");
-            }else if(jsonResponse.serverResponse === "Allowed") {
-                /* This is where I need work everytime as per the authentication filter*/
-
-                if( jsonResponse.classroomId == "Classroom not created" ){
-                    showClassroomFormRowErrorMessage( "Classroom creation was unsuccessful. Please try again later..." );
-                }else {
-                    createClassroomHtml( jsonResponse.classroomId );
-                    showClassroomFormRowErrorMessage();
-                }
-            }else{
-                alert("something went wrong!!!");
-            }
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+        if (this.readyState === 4 && this.status === 200){
+            completeLogin( this ) /*This is where we get the response when the request was successfully sent and a successfully response is received */
         }
     }
 
+    httpreq.open( "POST" , "/EduClick_war_exploded/Admin" , true);
+    httpreq.setRequestHeader("Content-type" , "application/x-www-form-urlencoded");
+    httpreq.send("textMsg=" + textMsg );
 
-    if( classroomName.value === "" || yearOfExamination.value === "" || gradeClass.value === "" || subject.value === "" ){
-        showClassroomFormRowErrorMessage( "Please fill all input fields..." );
+    function completeLogin( httpreq ){
 
-    }else {
-        sendServerData();
+        let jsonLoginResponse = JSON.parse(httpreq.responseText);
+
+        /*here when we receive the response
+        from the server, we convert it to JSON as it will be sent as JSON from the servlet.
+        Once we parse the response to JSON we use jsonLoginResponse.User to get the value of User member
+        in the JSON object specified by the servlet*/
+
+        /*if ( jsonLoginResponse.User === "Admin"){
+
+            window.location.replace("/EduClick_war_exploded/Admin/AdminHome-DashBoard.html");
+        }
+        else{
+            alert(" Something went wrong!");
+        }*/console.log(jsonLoginResponse);
     }
-
 }
 
-
+submitButton.onclick = function (){
+    sendServerData();
+}
