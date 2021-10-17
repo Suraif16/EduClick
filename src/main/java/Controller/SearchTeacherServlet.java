ca4459e486add730c525298a810e5b4fc7004eed
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-10-17 18:07:04 +0530

    add codes for search teacher

--- src/main/java/Controller/SearchTeacherServlet.java
+++ src/main/java/Controller/SearchTeacherServlet.java
@@ -1,5 +1,9 @@
 package Controller;
 
+import Model.Requests;
+import Model.Teacher;
+import Model.User;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.servlet.ServletException;
@@ -9,9 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchTeacherServlet extends HttpServlet {
-
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
@@ -20,11 +25,21 @@ public class SearchTeacherServlet extends HttpServlet {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("serverResponse" , "Allowed");
 
-        HttpSession session = request.getSession();
+        HttpSession session = request.getSession(false);
+        User user = (User) session.getAttribute("User");
+
+        List<String> teacherList = new ArrayList<>();
 
         String teacherName = request.getParameter("searchValue");
 
+       // Teacher teacherName = new Teacher(user);
+        //teacher.searchTeacher();
+
         System.out.println(teacherName);
+
+        JSONArray jsonArray = new JSONArray( teacherList );
+
+        jsonObject.put("teacherList" , jsonArray);
 
         out.write(jsonObject.toString());
         out.close();
