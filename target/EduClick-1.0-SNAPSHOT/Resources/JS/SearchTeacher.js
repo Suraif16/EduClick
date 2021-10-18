92cec05c306d85c818614e68676b35673113f41b
Farzan <farzanroxz123@gmail.com>
2021-10-18 18:27:33 +0530

    target files, and added         <load-on-startup>0</load-on-startup>

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/SearchTeacher.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/SearchTeacher.js
@@ -2,35 +2,44 @@ const searchValue = document.getElementById("searchBarText");
 
 searchValue.addEventListener( "keyup" , function (event){
 
-    const searchBarValue = document.getElementById("searchBarText").value;
-    console.log(searchBarValue);
-    searchForTeacher();
+    if(event.key === "Enter"){
+        searchForTeacher(searchValue);
+    }
 
 });
 
-function searchForTeacher(){
-
-    const searchBarValue = document.getElementById("searchBarText").value;
+function searchForTeacher(searchValue) {
 
     let httpreq = new XMLHttpRequest();
     httpreq.onreadystatechange = function (){
 
         if (this.readyState === 4 && this.status === 200){
-            completeSearch( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+
+            completeSearch( this );
         }
     }
-    /* ************************ */
-    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacher" , true);
+    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/searchTeacher" , true);
     httpreq.setRequestHeader("Content-type" , "application/x-www-form-urlencoded");
-    httpreq.send("searchBarText=" + searchBarValue);
-
-    function completeSearch(httpreq){
-
-        console.log(httpreq.responseText);
-        let jsonSearchResponse = JSON.parse(httpreq.responseText);
-
-    }
-
+    httpreq.send("searchValue=" + searchValue);
 
 
 }
+const completeSearch = function( httpreq ){
+
+    let jsonResponse = JSON.parse( httpreq.responseText);
+
+    if( jsonResponse.serverResponse === "null Session" || jsonResponse.serverResponse === "Not Allowed"){
+        window.location.replace("/EduClick_war_exploded/Login.html");
+    }else if(jsonResponse.serverResponse === "Allowed") {
+        /* This is where I need work everytime as per the authentication filter*/
+
+        console.log("jsonResponse");
+
+    }else{
+        alert("something went wrong!!!");
+    }
+
+    
+
+}
+
