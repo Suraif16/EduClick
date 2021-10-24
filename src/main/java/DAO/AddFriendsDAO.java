5afa90131f6be35474b21c056912bd972144bc12
Farzan <farzanroxz123@gmail.com>
2021-10-24 17:01:56 +0530

    created two sql methods in addfriendsdao and followsdao to check if users are following or already friends with each other

---
+++ src/main/java/DAO/AddFriendsDAO.java
@@ -0,0 +1,42 @@
+package DAO;
+
+import Database.DBConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AddFriendsDAO {
+
+    public Boolean checkIsFriend( String userId , String friendUserId ){
+
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+
+        try{
+
+            connection = dbConnectionPool.dataSource.getConnection();
+            String sql = "SELECT * FROM Add_Friends WHERE UserID = ? AND Friend_UserID = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            preparedStatement.setString( 1 , userId );
+            preparedStatement.setString( 2 , friendUserId );
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if ( resultSet.next() ){
+                return true;
+            }
+
+        }catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        finally {
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
+        }
+
+        return false;
+
+    }
+
+
+}
