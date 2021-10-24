a35165946bf4415087706b7f2a0aa54de2d5a32e
Farzan <farzanroxz123@gmail.com>
2021-10-24 13:15:04 +0530

    Adder two private variables in Teacher.java class with relavent getters and setters

--- src/main/java/Model/Teacher.java
+++ src/main/java/Model/Teacher.java
@@ -9,7 +9,8 @@ public class Teacher extends User{
     private String currentWorkingPlace;
     private List<String> subjects;
     private List<String> Qualifications;
-
+    private String friendStatus;
+    private String FollowStatus;
 
     /*Getters and setters begins here*/
 
@@ -35,6 +36,22 @@ public class Teacher extends User{
 
     public void setQualifications(List<String> qualifications) {
         Qualifications = qualifications;
+    }
+
+    public String getFriendStatus() {
+        return friendStatus;
+    }
+
+    public void setFriendStatus(String friendStatus) {
+        this.friendStatus = friendStatus;
+    }
+
+    public String getFollowStatus() {
+        return FollowStatus;
+    }
+
+    public void setFollowStatus(String followStatus) {
+        FollowStatus = followStatus;
     }
 
     /*Getters and setters ends here*/
