08813a27f99fef5c7e8c855d988bd1bf04f8f59d
Farzan <farzanroxz123@gmail.com>
2021-10-24 10:22:51 +0530

    completed the search servlet and added mapping in web.xml file

---
+++ src/main/java/Controller/SearchServlet.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class SearchServlet extends HttpServlet {
+
+    @Override
+    public void doGet( HttpServletRequest request , HttpServletResponse response ) throws IOException {
+
+        HttpSession session = request.getSession( false );
+        String searchValue = request.getParameter( "searchValue" );
+        session.setAttribute( "searchValue" , searchValue );
+        response.sendRedirect( "Search.html" );
+
+    }
+
+}
