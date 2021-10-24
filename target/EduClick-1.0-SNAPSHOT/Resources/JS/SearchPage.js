76638dba16da5caae058c7c934ed64d7845ffff6
Farzan <farzanroxz123@gmail.com>
2021-10-24 17:10:17 +0530

    target files

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/SearchPage.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/SearchPage.js
@@ -14,7 +14,10 @@ const logout = document.getElementById( "logout" );
 const logoAnchorLink = document.getElementById( "linkLogo" );
 const searchPost = document.getElementById( "searchPost" );
 const searchComment = document.getElementById( "searchComment" );
+const search = document.getElementById( "searchBarText" );
 
+let userNameValue;
+let userTypeValue;
 
 const searchPageLoad = function (){
 
@@ -48,23 +51,42 @@ const searchPageLoad = function (){
 
         }
 
+        userNameValue = jsonResponse.UserName;
+        userTypeValue = jsonResponse.UserType;
+
+        searchForTeacher();
+
     }
 }
 
 
 
-/*searchValue.addEventListener( "keyup" , function (event){
+search.addEventListener( "keyup" , function (event){
 
 
     if(event.key === "Enter"){
-        searchForTeacher(searchValue.value);
+
+        let httpreq = new XMLHttpRequest();
+
+        httpreq.onreadystatechange = function (){
+
+            if ( httpreq.readyState === 4 && httpreq.status === 200){
+                console
+                searchPageLoad();
+
+            }
+        }
+        let url = "/EduClick_war_exploded/Search?searchValue="+search.value;
+        httpreq.open( "GET" , url ,true);
+        httpreq.send();
+
     }
 
 });
 
 
 
-function searchForTeacher(searchValue) {
+function searchForTeacher() {
 
     let httpreq = new XMLHttpRequest();
     httpreq.onreadystatechange = function (){
@@ -74,31 +96,69 @@ function searchForTeacher(searchValue) {
             completeSearch( this );
         }
     }
-    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/searchTeacher" , true);
-    httpreq.setRequestHeader("Content-type" , "application/x-www-form-urlencoded");
-    httpreq.send("value=" + value);
-    console.log("ggghh" + value);
+    httpreq.open( "GET" , "/EduClick_war_exploded/searchTeacher" , true);
+    httpreq.send();
+
+    const completeSearch = function( httpreq ){
+        const searchContent = document.getElementById( "rightPanelStudentList" );
+        searchContent.innerHTML = "";
+        let jsonResponse = JSON.parse( httpreq.responseText);
+
+        console.log(jsonResponse.teacherList)
+        console.log(userTypeValue);
+        console.log(userNameValue)
+        
+        if( jsonResponse.teacherList.length > 0){
+
+            for ( i = 0 ; i < jsonResponse.teacherList.length ; i++ ){
+
+                let htmlString = '<div class="rightPanelSingleStudent" >' +
+                    '                <div>' +
+                    '                    <a href="/EduClick_war_exploded/userProfileRedirect?userId='+ jsonResponse.teacherList[i].userID +'" class="profile">' +
+                    '                        <div class="profileImage classroomStudentProfilePicture">' +
+                    '                            <img class="profileIcon" src="Resources/Icons/account_circle_white_24dp.svg">' +
+                    '                        </div>' +
+                    '                        <div class="classroomStudentProfileName">' + jsonResponse.teacherList[i].firstName + " " +jsonResponse.teacherList[i].lastName + '</div>' +
+                    '                    </a>' +
+                    '                </div>';
+
+                if( userTypeValue !== "Guest" ){
+
+                    if (userTypeValue === "Teacher"){
+
+                        htmlString += '                <div>' +
+                            '                    <input style="display:block;" id="enable2" type="button" value="Add Friend" onclick="enableDisableStatus(' + jsonResponse.teacherList[i].userID +')">' +
+                            '                    <input style="display:none;" id="disable2" type="button" value="Cancel request" class="studentDisable" onclick="enableDisableStatus(' + jsonResponse.teacherList[i].userID +')">' +
+                            '                </div>';
+
+                    }else if(userTypeValue === "Student"){
+
+                        htmlString += '                <div>' +
+                            '                    <input style="display:block;" id="enable2" type="button" value="Follow" onclick="enableDisableStatus(' + jsonResponse.teacherList[i].userID +')">' +
+                            '                    <input style="display:none;" id="disable2" type="button" value="Unfollow" class="studentDisable" onclick="enableDisableStatus(' + jsonResponse.teacherList[i].userID +')">' +
+                            '                </div>';
+
+                    }
+
+                }
+
+                htmlString += '</div>';
+
+                searchContent.innerHTML += htmlString;
+
+
+            }
+
+        }else {
+            console.log("no found")
+            searchContent.innerHTML = "No result found...";
+
+        }
+
+    }
 
 
 }
 
 
-const completeSearch = function( httpreq ){
 
-    let jsonResponse = JSON.parse( httpreq.responseText);
-
-    if( jsonResponse.serverResponse === "null Session" || jsonResponse.serverResponse === "Not Allowed"){
-        window.location.replace("/EduClick_war_exploded/Login.html");
-    }else if(jsonResponse.serverResponse === "Allowed") {
-        /!* This is where I need work everytime as per the authentication filter*!/
-
-
-        console.log("jsonResponse");
-
-    }else{
-        alert("something went wrong!!!");
-    }
-
-    
-
-}*/
