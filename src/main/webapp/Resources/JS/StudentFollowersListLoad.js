693ac611e2149d21f9a70274710fd9b3a0cbd039
Venushka <venushkachstc@gmail.com>
2021-10-29 00:53:57 +0530

    Modified js file to send server request

--- src/main/webapp/Resources/JS/StudentFollowersListLoad.js
+++ src/main/webapp/Resources/JS/StudentFollowersListLoad.js
@@ -1,3 +1,22 @@
 const loadFollowersList = function (){
-    console.log("mama load una bosa onna")
+    let httpreq = new XMLHttpRequest();
+
+    httpreq.onreadystatechange = function () {
+
+        if (this.readyState === 4 && this.status === 200) {
+            console.log("I am running")
+            completeLoad(this); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+
+        }
+    }
+
+    httpreq.open("POST", "/EduClick_war_exploded/studentFollowerListLoad", true);
+    httpreq.send();
+
+    function completeLoad(httpreq){
+        let jsonResponse = JSON.parse(httpreq.responseText);
+        console.log(jsonResponse.check)
+    }
+
+
 }
