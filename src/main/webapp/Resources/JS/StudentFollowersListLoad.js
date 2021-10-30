563ee76ab26563d8fe519344902f14c168e193b5
Venushka <venushkachstc@gmail.com>
2021-10-30 18:46:44 +0530

    Added the js script to send request to server

--- src/main/webapp/Resources/JS/StudentFollowersListLoad.js
+++ src/main/webapp/Resources/JS/StudentFollowersListLoad.js
@@ -84,3 +84,51 @@ const loadFollowersList = function (){
 
 
 }
+
+document.onreadystatechange = function (){
+
+    if ( document.readyState === 'complete' ){
+        /* when the document is loaded and complete this function will run*/
+        sendServerData();
+
+    }
+
+}
+
+const sendServerData = function (){
+    /* This function gets the username from the server*/
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+
+        if (this.readyState === 4 && this.status === 200){
+            completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/student/studentProfileNameLoad" , true);
+    httpreq.send();
+
+    function completeLogin( httpreq ){
+
+        let jsonLoginResponse = JSON.parse(httpreq.responseText);
+
+
+
+        if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
+            window.location.replace("/EduClick_war_exploded/Login.html");
+        }else if(jsonLoginResponse.serverResponse === "Allowed") {
+
+            console.log(jsonLoginResponse);
+            /* This is where I need work everytime as per the authentication filter*/
+            console.log(jsonLoginResponse.FullName);
+            const name = document.getElementById("profileUserName");
+            name.innerHTML = jsonLoginResponse.FullName;
+        }else{
+            alert("something went wrong!!!");
+        }
+
+    }
+
+
+}
