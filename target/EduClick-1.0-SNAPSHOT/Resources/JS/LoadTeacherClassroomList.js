48c7a61974b1097e9843f6a17339a5496169ecc9
Rahuram <rahuram66@gmail.com>
2021-10-30 21:09:55 +0530

    authentication insert

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/LoadTeacherClassroomList.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/LoadTeacherClassroomList.js
@@ -15,6 +15,8 @@ const sendD = function () {
 
         if (this.readyState === 4 && this.status === 200) {
             completeLoad(this); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+            sendNameData();
+            LoadTeacherProName();
         }
 
     }
@@ -70,4 +72,82 @@ const sendD = function () {
             '                </div>';
 
     }
+}
+
+const sendNameData = function (){
+    console.log("Firstname loaded!!")
+    /* This function gets the username from the server*/
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+
+        if (this.readyState === 4 && this.status === 200){
+            completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/student/studentLoad" , true);
+    httpreq.send();
+
+    function completeLogin( httpreq ){
+
+        let jsonLoginResponse = JSON.parse(httpreq.responseText);
+
+
+
+        if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
+            window.location.replace("/EduClick_war_exploded/Login.html");
+        }else if(jsonLoginResponse.serverResponse === "Allowed") {
+
+            console.log(jsonLoginResponse);
+            /* This is where I need work everytime as per the authentication filter*/
+            console.log(jsonLoginResponse.firstName);
+            const name = document.getElementById("headerUserName");
+            name.innerHTML = jsonLoginResponse.firstName;
+        }else{
+            alert("something went wrong!!!");
+        }
+
+    }
+
+
+}
+const LoadTeacherProName = function (){
+    console.log("Firstname loaded!!")
+    /* This function gets the username from the server*/
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+
+        if (this.readyState === 4 && this.status === 200){
+            completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/student/teacherProfileNameLoad" , true);
+    httpreq.send();
+
+    function completeLogin( httpreq ){
+
+        let jsonLoginResponse = JSON.parse(httpreq.responseText);
+        console.log("teacher pro name find loaded");
+
+
+
+
+        if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
+            window.location.replace("/EduClick_war_exploded/Login.html");
+        }else if(jsonLoginResponse.serverResponse === "Allowed") {
+
+            const name = document.getElementById("teacherUserName");
+            name.innerHTML = jsonLoginResponse.FullName;
+            console.log(name);
+
+        }else{
+            alert("something went wrong!!!");
+        }
+
+    }
+
+
 }
