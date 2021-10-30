62844bff88c93c1bc0c60c8304c937be65a855af
Farzan <farzanroxz123@gmail.com>
2021-10-30 20:23:43 +0530

    added an insert function to insert when a student follows a teacher

--- src/main/java/DAO/FollowsDAO.java
+++ src/main/java/DAO/FollowsDAO.java
@@ -38,4 +38,28 @@ public class FollowsDAO {
 
     }
 
+    public void insert( String S_UserId , String T_UserId ){
+
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+
+        try{
+
+            connection = dbConnectionPool.dataSource.getConnection();
+            String sql = "INSERT INTO Follows VALUES( ? , ? )";
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            preparedStatement.setString( 1 , S_UserId );
+            preparedStatement.setString( 2 , T_UserId );
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
+        }
+
+    }
+
 }
