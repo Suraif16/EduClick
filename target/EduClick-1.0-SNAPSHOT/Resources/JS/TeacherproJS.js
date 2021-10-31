c529262402cd4ea115dbc68e39b7954e76656ca7
Farzan <farzanroxz123@gmail.com>
2021-10-31 09:35:09 +0530

    target files, server restart

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/TeacherproJS.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/TeacherproJS.js
@@ -168,46 +168,7 @@ let sendData = function (id,action){
 
 
 }
-<<<<<<< HEAD
-=======
 
-
-const LoadName = function (){
-    
-    let httpreq = new XMLHttpRequest();
-    httpreq.onreadystatechange = function (){
->>>>>>> main
-
-        if (this.readyState === 4 && this.status === 200){
-            completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
-        }
-
-    }
-
-    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherProfileNameLoad" , true);
-    httpreq.send();
-
-    function completeLogin( httpreq ){
-
-        let jsonLoginResponse = JSON.parse(httpreq.responseText);
-
-        if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
-            window.location.replace("/EduClick_war_exploded/Login.html");
-        }else if(jsonLoginResponse.serverResponse === "Allowed") {
-
-            const name = document.getElementById("teacherUserName");
-            name.innerHTML = jsonLoginResponse.FullName;
-            console.log(name);
-
-            const headerName = document.getElementById("teacherUserNameHeader");
-            headerName.innerHTML = jsonLoginResponse.FullName;
-            console.log(headerName);
-
-        }else{
-            alert("something went wrong!!!");
-        }
-
-    }
 
 const LoadName = function (){
     
