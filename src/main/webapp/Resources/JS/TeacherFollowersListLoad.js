c0cc44a3765c2ac79aaeab2a490aaf477459852f
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-11-02 11:36:31 +0530

    change fumction for display user name

--- src/main/webapp/Resources/JS/TeacherFollowersListLoad.js
+++ src/main/webapp/Resources/JS/TeacherFollowersListLoad.js
@@ -1,11 +1,63 @@
+document.onreadystatechange = function (){
+
+    if ( document.readyState === 'complete' ){
+        /* when the document is loaded and complete this function will run*/
+        // sendServerData();
+        LoadUserName();
+
+    }
+
+}
+
+
+const LoadUserName = function (){
+
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+
+        if (this.readyState === 4 && this.status === 200){
+            completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherProfileNameLoad" , true);
+    httpreq.send();
+
+    function completeLogin( httpreq ){
+
+        let jsonLoginResponse = JSON.parse(httpreq.responseText);
+
+        if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
+            window.location.replace("/EduClick_war_exploded/Login.html");
+        }else if(jsonLoginResponse.serverResponse === "Allowed") {
+
+            const name = document.getElementById("teacherUserName");
+            name.innerHTML = jsonLoginResponse.FullName;
+            console.log(name);
+
+            const headerName = document.getElementById("teacherUserNameHeader");
+            headerName.innerHTML = jsonLoginResponse.FullName;
+            console.log(headerName);
+
+        }else{
+            alert("something went wrong!!!");
+        }
+
+    }
+
+
+}
+
 const loadTeacherFollowersList = function (){
-    console.log("HUUU");
+    console.log("1234");
+
     let httpreq = new XMLHttpRequest();
 
     httpreq.onreadystatechange = function () {
-        console.log("jjjjj");
+
         if (this.readyState === 4 && this.status === 200) {
-            console.log("I am running");
+            console.log("I am running")
             completeLoad(this); /*This is where we get the response when the request was successfully sent and a successfully response is received */
 
         }
@@ -26,9 +78,9 @@ const loadTeacherFollowersList = function (){
             let count = jsonLoginResponse.List.length - 1;
             for (i = 0 ; i <= count ; i++){
 
-
-                classroomHtmlOutput(jsonLoginResponse.List[i].UserID,jsonLoginResponse.List[i].lastName,jsonLoginResponse.List[i].firstName);
-
+                console.log("hiii");
+                classroomHtmlOutput(jsonLoginResponse.List[i].UserID,jsonLoginResponse.List[i].firstName,jsonLoginResponse.List[i].lastName);
+                console.log(jsonLoginResponse.list);
             }
         }else{
             alert("something went wrong!!!");
@@ -36,11 +88,38 @@ const loadTeacherFollowersList = function (){
     }
 
     function classroomHtmlOutput( userID , firstName , lastName){
+        console.log("very good");
+
         const postContents = document.getElementById("postContents");
         postContents.innerHTML = "";
-     //   const searchContent = document.getElementById( "rightPanelStudentList" );
+        const searchContent = document.getElementById( "rightPanelStudentList" );
         const rightPanel = document.getElementById("rightPanel");
         rightPanel.style.display = "flex";
+        rightPanel.style.width = "400%";
+        rightPanel.style.marginLeft = "270%";
+
+
+        /*let htmlString = '<div class="rightPanelSingleStudent" style=" flex-basis: 15%;' +
+            '    margin: 1% auto;' +
+            '    display: flex;' +
+            '    flex-direction: column;' +
+            '    width: 100%;' +
+            '    background-color: #4775c4;">' +
+            '                <div>' +
+            '                    <a href="/EduClick_war_exploded/userProfileRedirect?userId='+ userID +'" class="profile" style="display: flex;">' +
+            '                        <div class="classroomStudentProfileName">' + firstName + " " +lastName + '</div>' +
+            '                    </a>' +
+            '                </div>';
+
+        htmlString += '       <div>' +
+            '                    <input style="display:block;" id="follow'+ userID +'" type="button" value="Follow" onclick="followUnfollowTeachers(' + userID +')">' +
+            '                    <input style="display:none;" id="unFollow'+ userID +'" type="button" value="Unfollow" class="studentDisable" onclick="followUnfollowTeachers(' +userID +')">' +
+            '                </div>';
+
+
+        htmlString += '</div>';
+
+        searchContent.innerHTML += htmlString;*/
 
 
         let htmlString = '<div id="rightPanel" style="margin: 5.6% auto 5.6% auto;' +
@@ -75,61 +154,55 @@ const loadTeacherFollowersList = function (){
 
 
 
-
-
-
-
-
-
     }
 
 
 }
 
-document.onreadystatechange = function (){
-
-    if ( document.readyState === 'complete' ){
-        /* when the document is loaded and complete this function will run*/
-       // sendServerData();
-
-    }
-
-}
-
-const sendServerData = function (){
-    /* This function gets the username from the server*/
-    let httpreq = new XMLHttpRequest();
-    httpreq.onreadystatechange = function (){
-
-        if (this.readyState === 4 && this.status === 200){
-            completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
-        }
-
-    }
-
-    httpreq.open( "POST" , "/EduClick_war_exploded/student/studentProfileNameLoad" , true);
-    httpreq.send();
-
-    function completeLogin( httpreq ){
-
-        let jsonLoginResponse = JSON.parse(httpreq.responseText);
-
-
-
-        if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
-            window.location.replace("/EduClick_war_exploded/Login.html");
-        }else if(jsonLoginResponse.serverResponse === "Allowed") {
-
-            console.log(jsonLoginResponse);
-            /* This is where I need work everytime as per the authentication filter*/
-            console.log(jsonLoginResponse.FullName);
-            const name = document.getElementById("profileUserName");
-            name.innerHTML = jsonLoginResponse.FullName;
-        }else{
-            alert("something went wrong!!!");
-        }
-
-    }
-
-
-}
+// document.onreadystatechange = function (){
+//
+//     if ( document.readyState === 'complete' ){
+//         /* when the document is loaded and complete this function will run*/
+//         sendServerData();
+//
+//     }
+//
+// }
+//
+// const sendServerData = function (){
+//     /* This function gets the username from the server*/
+//     let httpreq = new XMLHttpRequest();
+//     httpreq.onreadystatechange = function (){
+//
+//         if (this.readyState === 4 && this.status === 200){
+//             completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+//         }
+//
+//     }
+//
+//     httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherProfileNameLoad" , true);
+//     httpreq.send();
+//
+//     function completeLogin( httpreq ){
+//
+//         let jsonLoginResponse = JSON.parse(httpreq.responseText);
+//
+//
+//
+//         if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
+//             window.location.replace("/EduClick_war_exploded/Login.html");
+//         }else if(jsonLoginResponse.serverResponse === "Allowed") {
+//
+//             console.log(jsonLoginResponse);
+//             /* This is where I need work everytime as per the authentication filter*/
+//             console.log(jsonLoginResponse.FullName);
+//             const name = document.getElementById("profileUserName");
+//             name.innerHTML = jsonLoginResponse.FullName;
+//         }else{
+//             alert("something went wrong!!!");
+//         }
+//
+//     }
+//
+//
+// }
