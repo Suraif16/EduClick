eebe81f7b078a5047258798935fc7a9138924e7d
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-11-03 18:50:46 +0530

    create function

--- src/main/java/Controller/UserProfile/TeacherFriendsListLoadServlet.java
+++ src/main/java/Controller/UserProfile/TeacherFriendsListLoadServlet.java
@@ -1,4 +1,21 @@
 package Controller.UserProfile;
 
-public class TeacherFriendsListLoadServlet {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TeacherFriendsListLoadServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request , HttpServletResponse response) throws IOException{
+
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+
+        HttpSession session = request.getSession( false );
+        
+    }
+
 }
