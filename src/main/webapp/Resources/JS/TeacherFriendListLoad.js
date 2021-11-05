c0d6af02e94a74ac51c25972f3e4b913a2d6cbd0
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-11-05 11:12:07 +0530

    create a js file for display friends:

--- src/main/webapp/Resources/JS/TeacherFriendListLoad.js
+++ src/main/webapp/Resources/JS/TeacherFriendListLoad.js
@@ -1,51 +1,4 @@
-document.onreadystatechange = function (){
-
-    if ( document.readyState === 'complete' ){
-
-        LoadUserName();
-
-    }
-
-}
-
-
-const LoadUserName = function (){
-
-    let httpreq = new XMLHttpRequest();
-    httpreq.onreadystatechange = function (){
-
-        if (this.readyState === 4 && this.status === 200){
-            completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
-        }
-
-    }
-
-    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherProfileNameLoad" , true);
-    httpreq.send();
-
-    function completeLogin( httpreq ){
-
-        let jsonLoginResponse = JSON.parse(httpreq.responseText);
-
-        if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
-            window.location.replace("/EduClick_war_exploded/Login.html");
-        }else if(jsonLoginResponse.serverResponse === "Allowed") {
-
-            const headerName = document.getElementById("teacherUserNameHeader");
-            headerName.innerHTML = jsonLoginResponse.FullName;
-            console.log(headerName);
-
-        }else{
-            alert("something went wrong!!!");
-        }
-
-    }
-
-
-}
-
 const loadTeacherFriendList = function (){
-
     console.log("1234");
 
     let httpreq = new XMLHttpRequest();
@@ -54,7 +7,7 @@ const loadTeacherFriendList = function (){
 
         if (this.readyState === 4 && this.status === 200) {
             console.log("I am running")
-            completeLoad(this); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+            LoadFriends(this); /*This is where we get the response when the request was successfully sent and a successfully response is received */
 
         }
     }
@@ -62,7 +15,7 @@ const loadTeacherFriendList = function (){
     httpreq.open("POST", "/EduClick_war_exploded/teacher/teacherFriendsListLoad", true);
     httpreq.send();
 
-    function completeLoad(httpreq){
+    function LoadFriends(httpreq){
         let jsonLoginResponse = JSON.parse(httpreq.responseText);
         console.log(jsonLoginResponse)
         if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
@@ -134,5 +87,6 @@ const loadTeacherFriendList = function (){
 
 
     }
+
 
 }
