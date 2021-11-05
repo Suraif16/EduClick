9fe5ad2acfe885ec8e517418c46e73831977b03b
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-11-05 12:52:43 +0530

    edit css part

--- src/main/webapp/Resources/JS/TeacherFriendListLoad.js
+++ src/main/webapp/Resources/JS/TeacherFriendListLoad.js
@@ -47,16 +47,16 @@ const loadTeacherFriendList = function (){
         const searchContent = document.getElementById( "rightPanelStudentList" );
         const rightPanel = document.getElementById("rightPanel");
         rightPanel.style.display = "flex";
-        rightPanel.style.width = "400%";
-        rightPanel.style.marginLeft = "270%";
+        rightPanel.style.width = "40%";
+        rightPanel.style.marginLeft = "30%";
 
         console.log("abc-1");
 
-
+/*
         const htmlString = '<div id="rightPanel" style="margin: 5.6% auto 5.6% auto;' +
             '    box-shadow:  inset -1px -1px 5px #4775c4 , inset 1px 1px 5px #4775c4 ;' +
             'left: 40%;' +
-            '    width: 90%;' +
+            '    width: 900%;' +
             '    padding: 1%;' +
             '    display: flex;' +
             '    flex-direction: column;' +
@@ -77,6 +77,30 @@ const loadTeacherFriendList = function (){
             '            </div>' +
             '        </div>'
         console.log("ggg");
+        rightPanel.innerHTML +=htmlString;*/
+
+        let htmlString = '<div id="rightPanel" style="margin: 2% auto 2% auto;' +
+            'left: 400%;' +
+            '    width: 90%;' +
+            '    padding: 1%;' +
+            '    display: flex;' +
+            '    flex-direction: column;' +
+            '    max-height: 76%;">' +
+            '            <div id="rightPanelStudentList" style="color:#4775c4" >' +
+            '                    <div id="innerDiv" style="display: inline-flex;">' +
+            '                        <a id="innerA" href="/EduClick_war_exploded/userProfileRedirect?userId='+ userID +'" class="profile" class="profile" style="flex: 1;display: flex;">' +
+            '                            <div class="profileImage classroomStudentProfilePicture" >' +
+            '                                <img class="profileIcon" src="../Resources/Icons/account_circle_white_24dp.svg"  >' +
+            '                            </div>' +
+            '                            <div class="classroomStudentProfileName" style="color:#4775c4">' + firstName + " " +lastName + '</div>' +
+            '                        </a>' +
+            '                    </div>' +
+            '                    <div>' +
+            '                        <input style="display:none; width: 100%;" id="addFriend'+userID+'" type="button" value="Add Friend" onclick="addFriendCancel(' + userID +')">' +
+            '                        <input style="display:block;width: 100%;" id="cancelRequest'+userID+'" type="button" value="Unfriend" class="studentDisable" onclick="addFriendCancel(' +userID +')">' +
+            '                </div>' +
+            '            </div>' +
+            '        </div>'
         rightPanel.innerHTML +=htmlString;
 
 
