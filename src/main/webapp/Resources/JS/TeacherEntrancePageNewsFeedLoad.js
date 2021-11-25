70708bf3326b0bff2aabe770ff982273fb99d24d
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-11-26 02:03:38 +0530

    create js for laod news feed in entrance page

---
+++ src/main/webapp/Resources/JS/TeacherEntrancePageNewsFeedLoad.js
@@ -0,0 +1,48 @@
+document.onreadystatechange = function (){
+
+    if ( document.readyState === 'complete' ){
+
+        sendServerDataTeacher();
+        console.log("kkk");
+
+    }
+
+}
+
+const sendServerDataTeacher = function (){
+
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+
+        if (this.readyState === 4 && this.status === 200){
+            completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherEntrancePageNewsFeedLoad" , true);
+    httpreq.send();
+
+    function completeLogin( httpreq ){
+
+        const headerUserProfileIdAchorElement = document.getElementById("headerUserProfileId");
+
+        let jsonLoginResponse = JSON.parse(httpreq.responseText);
+
+        if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
+            window.location.replace("/EduClick_war_exploded/Login.html");
+        }else if(jsonLoginResponse.serverResponse === "Allowed") {
+            /* This is where I need work everytime as per the authentication filter*/
+            const name = document.getElementById("headerUserName");
+            name.innerHTML = jsonLoginResponse.firstName;
+            let url = '/EduClick_war_exploded/userProfileRedirect?userId=' + jsonLoginResponse.userId;
+            console.log(url);
+            headerUserProfileIdAchorElement.setAttribute("href" , url);
+        }else{
+            alert("something went wrong!!!");
+        }
+
+    }
+
+
+}
