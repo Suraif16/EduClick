4b62dc4827b0ac98fffa852c7fd0c3d7276b2e12
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-11-26 21:28:23 +0530

    load nf owner id

--- src/main/webapp/Resources/JS/TeacherEntrancePageNewsFeedLoad.js
+++ src/main/webapp/Resources/JS/TeacherEntrancePageNewsFeedLoad.js
@@ -26,19 +26,18 @@ const sendServerDataTeacher = function (){
 
     function completeLog( httpreq ){
 
-        const headerUserProfileIdAchorElement = document.getElementById("headerUserProfileId");
+       // const headerUserProfileIdAchorElement = document.getElementById("headerUserProfileId");
 
         let jsonLoginResponse = JSON.parse(httpreq.responseText);
 
         if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
             window.location.replace("/EduClick_war_exploded/Login.html");
         }else if(jsonLoginResponse.serverResponse === "Allowed") {
-            /* This is where I need work everytime as per the authentication filter*/
-            const name = document.getElementById("headerUserName");
-            name.innerHTML = jsonLoginResponse.firstName;
-            let url = '/EduClick_war_exploded/userProfileRedirect?userId=' + jsonLoginResponse.userId;
-           // console.log(url);
-            headerUserProfileIdAchorElement.setAttribute("href" , url);
+
+            const headerName = document.getElementById("headerUserName");
+            headerName.innerHTML = jsonLoginResponse.FullName;
+            console.log(headerName);
+
         }else{
             alert("something went wrong!!!");
         }
@@ -48,13 +47,14 @@ console.log("aaaa");
 
 }
 
+
 const LoadTUserName = function (){
 
     let httpreq = new XMLHttpRequest();
     httpreq.onreadystatechange = function (){
 
         if (this.readyState === 4 && this.status === 200){
-            completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+            completeLogin( this );
         }
 
     }
@@ -82,3 +82,4 @@ const LoadTUserName = function (){
 
 
 }
+
