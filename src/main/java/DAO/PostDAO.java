cd9c4696614f1f20c566121c718613d4943f6bc7
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-11-26 21:26:21 +0530

    create a object for load nf id

--- src/main/java/DAO/PostDAO.java
+++ src/main/java/DAO/PostDAO.java
@@ -8,10 +8,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class PostDAO {
-    public ArrayList<String> getNewsFeedsTeacherKeys(String userId){
+    public ArrayList<String> getNewsFeedsKeys(String userId){
         DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
         Connection connection = null;
-        ArrayList<String> NFTeacherKeyList = new ArrayList<String>();
+        ArrayList<String> NFKeyList = new ArrayList<String>();
         
         try {
             connection = dbConnectionPool.dataSource.getConnection();
@@ -21,9 +21,9 @@ public class PostDAO {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while(resultSet.next()){
-                NFTeacherKeyList.add(resultSet.getString("NFPostID"));
+                NFKeyList.add(resultSet.getString("NFPostID"));
             }
-            System.out.println(NFTeacherKeyList+"*************");
+            System.out.println(NFKeyList+"*************");
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -33,8 +33,38 @@ public class PostDAO {
             if (connection != null) try { connection.close(); }catch (Exception ignore) {}
 
         }
-        return NFTeacherKeyList;
+        return NFKeyList;
     }
+
+    //******
+    public ArrayList<String> getNFTeacherKeys(String userId){
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+        ArrayList<String> NFKeyList = new ArrayList<String>();
+
+        try {
+            connection = dbConnectionPool.dataSource.getConnection();
+            String sql = "SELECT T_UserID FROM Posts WHERE UserID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            preparedStatement.setString(1,userId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()){
+                NFKeyList.add(resultSet.getString("T_UserID"));
+            }
+            System.out.println(NFKeyList+"fffff*************");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
+
+        }
+        return NFKeyList;
+    }
+
 
 
 
