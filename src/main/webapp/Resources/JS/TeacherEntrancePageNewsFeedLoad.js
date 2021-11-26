8afe80ed22c2ab76ad269810229366a10daa4c9e
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-11-26 17:28:18 +0530

    load news feeds owners id

--- src/main/webapp/Resources/JS/TeacherEntrancePageNewsFeedLoad.js
+++ src/main/webapp/Resources/JS/TeacherEntrancePageNewsFeedLoad.js
@@ -1,7 +1,7 @@
 document.onreadystatechange = function (){
 
     if ( document.readyState === 'complete' ){
-
+        LoadTUserName();
         sendServerDataTeacher();
         console.log("kkk");
 
@@ -15,7 +15,7 @@ const sendServerDataTeacher = function (){
     httpreq.onreadystatechange = function (){
 
         if (this.readyState === 4 && this.status === 200){
-            completeLogin( this );
+            completeLog( this );
         }
 
     }
@@ -23,7 +23,8 @@ const sendServerDataTeacher = function (){
     httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherEntrancePageNewsFeedsLoad" , true);
     httpreq.send();
 
-    function completeLogin( httpreq ){
+
+    function completeLog( httpreq ){
 
         const headerUserProfileIdAchorElement = document.getElementById("headerUserProfileId");
 
@@ -36,7 +37,7 @@ const sendServerDataTeacher = function (){
             const name = document.getElementById("headerUserName");
             name.innerHTML = jsonLoginResponse.firstName;
             let url = '/EduClick_war_exploded/userProfileRedirect?userId=' + jsonLoginResponse.userId;
-            console.log(url);
+           // console.log(url);
             headerUserProfileIdAchorElement.setAttribute("href" , url);
         }else{
             alert("something went wrong!!!");
@@ -44,5 +45,40 @@ const sendServerDataTeacher = function (){
 
     }
 console.log("aaaa");
+
+}
+
+const LoadTUserName = function (){
+
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+
+        if (this.readyState === 4 && this.status === 200){
+            completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherProfileNameLoad" , true);
+    httpreq.send();
+
+    function completeLogin( httpreq ){
+
+        let jsonLoginResponse = JSON.parse(httpreq.responseText);
+
+        if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
+            window.location.replace("/EduClick_war_exploded/Login.html");
+        }else if(jsonLoginResponse.serverResponse === "Allowed") {
+
+            const headerName = document.getElementById("headerUserName");
+            headerName.innerHTML = jsonLoginResponse.FullName;
+            console.log(headerName);
+
+        }else{
+            alert("something went wrong!!!");
+        }
+
+    }
+
 
 }
