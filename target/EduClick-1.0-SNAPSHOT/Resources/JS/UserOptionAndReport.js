320bf67c9109a832f2c189967f9eb6a575806cb8
Farzan <farzanroxz123@gmail.com>
2021-12-06 21:19:09 +0530

    added a basic function to move the option menu to the require place

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/UserOptionAndReport.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/UserOptionAndReport.js
@@ -1,14 +1,23 @@
 
 const showOptionMenu = function ( id , type ){
 
-    const optionMenu = document.getElementById("userOptionMenu")
+    const optionMenu = document.getElementById("userOptionMenu");
+
+    if ( optionMenu.innerHTML === ""){
+
+        console.log( "Yes" );
+
+    }
 
     console.log( id , type );
     let element = document.getElementById( type + id )
     let elementLocation = element.getBoundingClientRect();
+    let bodyElement = document.getElementById("post1").getBoundingClientRect();
     console.log( elementLocation.top , elementLocation.bottom , elementLocation.right , elementLocation.left );
-    optionMenu.style.position = "fixed";
-    optionMenu.style.top = elementLocation.top;
-    optionMenu.style.left = elementLocation.left;
+    optionMenu.style.position = "absolute";
+    let topValue = elementLocation.top + window.scrollY /*- bodyElement.top*/;
+    let leftValue = elementLocation.left + window.scrollX /*- bodyElement.left*/;
+    console.log( topValue , leftValue );
+    optionMenu.style.margin = topValue +"px" + " auto auto "+ leftValue +"px";
 
 }
