7e5152b9de6e6c916d038908ef955017b94e5636
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-12-07 10:45:50 +0530

    add attachment icon for all posts

--- src/main/webapp/Resources/JS/ClassroomJS.js
+++ src/main/webapp/Resources/JS/ClassroomJS.js
@@ -47,26 +47,34 @@ function enableDisableStatus( id ){
 
 function showAnswers( id ){
 
-    let com = document.getElementById("ans");
+  /*  let com = document.getElementById("ans");
     if(com.style.display === "none"){
 
         com.style.display = "flex";
     }else{
         com.style.display = "none"
-    }
+    }*/
 
     let answerId = "answersInPost" + id;
     let answerContainer = document.getElementById( answerId );
+    let com = document.getElementById("ans" + id) ;
 
     if (answerContainer.style.display === "none"){
 
         answerContainer.style.display = "flex";
 
-    }else{
+        com.style.display = "flex";
+
+        }
+
+
+    else{
 
         answerContainer.style.display = "none";
+        com.style.display = "none"
+}
 
-    }
+
 
 
 }
