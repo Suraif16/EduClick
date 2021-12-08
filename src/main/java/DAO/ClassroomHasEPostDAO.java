a540a28061b8c982dd016958716ae4d043b29765
Farzan <farzanroxz123@gmail.com>
2021-12-08 17:51:34 +0530

    created and insert statment to ClassroomHasEPostDAO

--- src/main/java/DAO/ClassroomHasEPostDAO.java
+++ src/main/java/DAO/ClassroomHasEPostDAO.java
@@ -1,4 +1,36 @@
 package DAO;
 
+import Database.DBConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ClassroomHasEPostDAO {
+
+    public void insert( String classroomId , String EpostID ){
+
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+
+        try {
+
+            connection = dbConnectionPool.dataSource.getConnection();
+            String sql = "INSERT INTO Classroom_Has_EPost VALUES( ? , ? );";
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            preparedStatement.setString( 1 , classroomId );
+            preparedStatement.setString( 2 , EpostID );
+            preparedStatement.execute();
+
+            preparedStatement.close();
+
+        }catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        finally {
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
+        }
+
+    }
+
 }
