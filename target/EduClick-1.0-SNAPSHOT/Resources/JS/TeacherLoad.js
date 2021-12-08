c21bc9a0b48c387084f2ef511069ea185a1b2e18
Farzan <farzanroxz123@gmail.com>
2021-12-08 22:38:59 +0530

    adjust htmls in classroomjs.js file and teacherload.js, and its working

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/TeacherLoad.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/TeacherLoad.js
@@ -94,7 +94,7 @@ const getClassroomList = function (){
     function classroomHtmlOutput( classroomId , classroomName , subject , gradeClass , yearOfExamination ){
         classroomsListLinksSelect.innerHTML += '<div class="classroomsListLinksItems">' +
             '' +
-            '                        <a href="Classroom.html"  class="classRooms">' +
+            '                        <a href="/EduClick_war_exploded/SaveClassroomId?' + classroomId + '"  class="classRooms">' +
             '' +
             '                            <p>Classroom Name : ' + classroomName +'</p>' +
             '                            <p>Subject : ' + subject + '</p>' +
