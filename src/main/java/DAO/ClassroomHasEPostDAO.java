e0a3749f677fd41db01a0443f7d7e8804931a2e4
Venushka <venushkachstc@gmail.com>
2021-12-08 19:11:29 +0530

    Added the query to retrive data from classroom_has_epost table

--- src/main/java/DAO/ClassroomHasEPostDAO.java
+++ src/main/java/DAO/ClassroomHasEPostDAO.java
@@ -1,4 +1,39 @@
 package DAO;
 
+import Database.DBConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class ClassroomHasEPostDAO {
+    public ArrayList<String> getEpostsIds(String classroomId){
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+        ArrayList<String> ePostIdList = new ArrayList<String>();
+
+        try {
+            connection = dbConnectionPool.dataSource.getConnection();
+            String sql = "SELECT EPostID FROM Classroom_has_Epost WHERE ClassroomID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, classroomId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                ePostIdList.add(resultSet.getString("EPostID"));
+            }
+            /*for(int i=0;i<ePostIdList.size();i++){
+                System.out.println("Epost ID "+i+" : "+ePostIdList.get(i)+"\n");
+            }*/
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (connection != null) try { connection.close(); } catch (Exception ignore) {            }
+        }
+        return ePostIdList;
+    }
 }
