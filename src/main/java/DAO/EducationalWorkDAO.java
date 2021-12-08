261c0766d89c7f45be4c1184c42c1704873da3ec
Farzan <farzanroxz123@gmail.com>
2021-12-08 17:37:09 +0530

    created a insert function for educatoinal workDAO

--- src/main/java/DAO/EducationalWorkDAO.java
+++ src/main/java/DAO/EducationalWorkDAO.java
@@ -1,7 +1,49 @@
 package DAO;
 
+import Database.DBConnectionPool;
+import Model.EducationalWork;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class EducationalWorkDAO {
 
+    public String insert( EducationalWork educationalWork ){
 
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+
+        try{
+
+            connection = dbConnectionPool.dataSource.getConnection();
+            String sql = "INSERT INTO EducationalWork( EPostID , Caption) VALUES( ? , ? );";
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            preparedStatement.setString( 1 , educationalWork.getPostID() );
+            preparedStatement.setString( 2 , educationalWork.getCaption() );
+
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+
+            if ( resultSet.next() ){
+
+                return resultSet.getString( 1 );
+
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+
+        }catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        finally {
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
+        }
+
+        return  null;
+
+    }
 
 }
