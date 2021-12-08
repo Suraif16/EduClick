468516a27a9d41541e751ea731555fe4be2abf3f
Rahuram <rahuram66@gmail.com>
2021-12-08 09:12:30 +0530

    creating new function for admin post dao

--- src/main/java/DAO/AdminPostDAO.java
+++ src/main/java/DAO/AdminPostDAO.java
@@ -2,11 +2,15 @@ package DAO;
 
 import Database.DBConnectionPool;
 import Model.AdminPost;
-import Model.Classroom;
+import Model.User;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.sql.*;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class AdminPostDAO {
@@ -54,46 +58,48 @@ public class AdminPostDAO {
         return generatedSysPostUserId;
     }
 
-    public ArrayList<AdminPost> selectPostDetails(ArrayList<String> arrayList) {
-
+    public JSONArray getAPostDetails(ArrayList<String> APostIDList) {
         DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
         Connection connection = null;
+        ArrayList<User> NewsFeedsDetails = new ArrayList<>();
+        JSONArray jsonArray = new JSONArray();
 
-        ArrayList<AdminPost> postDetails = new ArrayList<AdminPost>();
 
         try {
             connection = dbConnectionPool.dataSource.getConnection();
 
-            for(int i=0;i<arrayList.size();i++) {
+            for (int i = 0; i < APostIDList.size(); i++) {
                 String sql = "select  SysPostID,APTextMsg, APTime, APDate from Admin_Post_System_Updates where SysPostID = ?";
+
                 PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setString(1, arrayList.get(i));
+                preparedStatement.setString(1, APostIDList.get(i));
+
                 ResultSet resultSet = preparedStatement.executeQuery();
                 while (resultSet.next()) {
-
                     String postId = resultSet.getString("SysPostID");
                     String textmsg = resultSet.getString("APTextMsg");
                     String date = resultSet.getString("APDate");
                     String time = resultSet.getString("APTime");
 
-                    AdminPost adminpost = new AdminPost(textmsg,date,time);
-                    adminpost.setSysPostID(postId);
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("Caption",textmsg);
+                    jsonObject.put("Date",date);
+                    jsonObject.put("Time",time);
+
+                    jsonArray.put(jsonObject);
 
                 }
-
-                resultSet.close();
-                preparedStatement.close();
-
             }
 
-
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         } finally {
-            if (connection != null) try { connection.close(); } catch (Exception ignore) {            }
-        }
-        return postDetails;
-    }
+            if (connection != null) try {
+                connection.close();
+            } catch (Exception ignore) {
+            }
 
+        }
+        return jsonArray;
+    }
 }
