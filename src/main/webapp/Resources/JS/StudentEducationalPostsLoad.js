4159b24a3148fa47b03712e263eb7276e529eb0a
Venushka <venushkachstc@gmail.com>
2021-12-09 13:41:43 +0530

    Added code lines to display the results dynamically

--- src/main/webapp/Resources/JS/StudentEducationalPostsLoad.js
+++ src/main/webapp/Resources/JS/StudentEducationalPostsLoad.js
@@ -14,6 +14,7 @@ const loadStudentEducationalPosts = function (){
     function completePostLoad( httpreq ){
 
         let jsonLoginResponse = JSON.parse(httpreq.responseText);
+        // console.log(jsonLoginResponse.TeacherFullName)
 
 
 
@@ -28,57 +29,80 @@ const loadStudentEducationalPosts = function (){
             }*/
             console.log(jsonLoginResponse.EPostContent.length)
 
-            /*for( i = 0 ; i < jsonLoginResponse.EPostContent.length ; i++){
-                let htmlString =
-                    '        <div class="post">    ' +
-                    '<div class="postContentContainer">\n' +
-                    '                <div class="postData">\n' +
-                    '                       <div class="postMessage">' + jsonLoginResponse.EPostContent[i].eWork.Caption +
+            for( i = 0 ; i < jsonLoginResponse.EPostContent.length ; i++){
+                if(jsonLoginResponse.EPostContent[i].ePost.Type == "Messages"){
+                    let htmlString =
+                        '<div class="post">' +
+                        '            <div class="postContentContainer">' +
+                        '                <div class="postProfileSection">' +
+                        '                    <a href="#" class="postProfile">' +
+                        '                        <div class="postProfileImage">' +
+                        '                            <img class="postProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg">' +
+                        '                        </div>\n' +
+                        '                        <div class="postProfileName" >'+jsonLoginResponse.TeacherFullName+'</div>' +
+                        '                        <div class="postTimeAndDate">' +
+                                                    jsonLoginResponse.EPostContent[i].ePost.Time+ ' | '+ jsonLoginResponse.EPostContent[i].ePost.Date +
+                        '                        </div>\n' +
+                        '                        <div class="userOptions">' +
+                        '                            <input class="userOptionsButton" type="button" value="    " id="postOption4" onclick="showOptionMenu(4,\'postOption\')">' +
+                        '                        </div>' +
+                        '                    </a>' +
+                        '                </div>' +
+                        '            </div>' +
+                        '            <div class="postContentContainer">' +
+                        '                <div class="postData">' +
+                        '                    <div class="postMessage">' +
+                        '                        <!--<div>-->' +
+                                                    jsonLoginResponse.EPostContent[i].eWork.Caption +
+                        '                        <!--</div>-->' +
+                        '                    </div>' +
+                        '                    <div class="postPicture">' +
+                        '                        <!--To only present the message without the picture, comment the part below-->' +
+                        '                        <!--<div class="postPictureImageContainer">' +
+                        '                            <img class="postPictureImage" src="Icons/seminar-text.jpg">' +
+                        '                        </div>-->' +
+                        '                    </div>' +
+                        '                    <div class="postContentContainer">' +
+                        '                        <div class="postAnswerButton">' +
+                        '                            <div class="answerButton" >' +
+                        '                                <input type="button" value="Answer" class="answerShowButton" onclick="showAnswers(4)">' +
+                        '                            </div>' +
+                        '                        </div>' +
+                        '                        <div class="postContentContainer">' +
+                        '                            <div style="display:none;" class="answersInPost" id="answersInPost4" >' +
+                        '                                <div class="singleAnswer">' +
+                        '                                    <div class="textAnswers" id="myComment">' +
+                        '                                        This is my Answer !!!!!!!!!' +
+                        '                                    </div>' +
+                        '                                        <a href="#">' +
+                        '                                            <img src="../Resources/Images/answers2.jpg">' +
+                        '                                        </a>' +
+                        '                                    </div>' +
+                        '                                    <div class="Marks">' +
+                        '                                        Your marks is 50%' +
+                        '                                    </div>' +
+                        '                                </div>' +
+                        '                            </div>' +
+                        '                        </div>' +
+                        '                        <div style="display: none" class="Marks" id="ans4" onclick="showAnswers(4)">' +
+                        '                            <div class="attachment">' +
+                        '                                <label class="attachmentLabel">' +
+                        '                                    <img id="attachmentImage" src="../Resources/Icons/outline_attach_file_white_24dp.png">' +
+                        '                                    <input type="file" accept="image/*">' +
+                        '                                </label>' +
+                        '                                <input id="comment" type="text" placeholder="   Comments...">' +
+                        '                                <input type="button" value="Post" class="answerShowButton">' +
+                        '                                <input type="button" value="Delete Answer" class="answerShowButton">' +
+                        '                            </div>' +
+                        '                        </div>' +
+                        '                    </div>' +
+                        '                </div>' +
+                        '            </div>' +
+                        '        </div>'
 
-                    '                       </div>' +
-                    '                    <div class="postPicture">\n' +
-                    '                    </div>\n' +
-                    '                    <div class="postContentContainer">\n' +
-                    '                        <div class="postAnswerButton">\n' +
-                    '                            <div class="answerButton" >\n' +
-                    '                                <input type="button" value="Answer" class="answerShowButton" onclick="showAnswers(4)">\n' +
-                    '                            </div>\n' +
-                    '                        </div>\n' +
-                    '                        <div class="postContentContainer">\n' +
-                    '                            <div style="display:none;" class="answersInPost" id="answersInPost4" >\n' +
-                    '                                <div class="singleAnswer">\n' +
-                    '                                    <div class="textAnswers" id="myComment">\n' +
-                    '                                        This is my Answer !!!!!!!!!\n' +
-                    '                                    </div>\n' +
-                    '                                    <div class="pictureAnswers">\n' +
-                    '                                        <a href="#">\n' +
-                    '                                            <img src="../Resources/Images/answers2.jpg">\n' +
-                    '                                        </a>\n' +
-                    '                                    </div>\n' +
-                    '                                    <div class="Marks">\n' +
-                    '                                        Your marks is 50%\n' +
-                    '                                    </div>\n' +
-                    '                                </div>\n' +
-                    '                            </div>\n' +
-                    '                        </div>\n' +
-                    '                        <div style="display: none" class="Marks" id="ans4" onclick="showAnswers(4)">\n' +
-                    '                            <div class="attachment">\n' +
-                    '                                <label class="attachmentLabel">\n' +
-                    '                                    <img id="attachmentImage" src="../Resources/Icons/outline_attach_file_white_24dp.png">\n' +
-                    '                                    <input type="file" accept="image/!*">\n' +
-                    '                                </label>\n' +
-                    '                                <input id="comment" type="text" placeholder="   Comments...">\n' +
-                    '                                <input type="button" value="Post" class="answerShowButton">\n' +
-                    '                                <input type="button" value="Delete Answer" class="answerShowButton">\n' +
-                    '                            </div>\n' +
-                    '                        </div>\n' +
-                    '                    </div>\n' +
-                    '                </div>\n' + +
-                    '            </div>'+
-                '                   </div>'
-
-                postContents.innerHTML += htmlString;
-            }*/
+                    postContents.innerHTML += htmlString;
+                }
+            }
 
 
 
