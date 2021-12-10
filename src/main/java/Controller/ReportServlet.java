46ae9b91634d77db20e548d43fbc2969dcf2f293
Rahuram <rahuram66@gmail.com>
2021-12-10 11:46:03 +0530

    finishing report servlet funtion with class

--- src/main/java/Controller/ReportServlet.java
+++ src/main/java/Controller/ReportServlet.java
@@ -18,8 +18,6 @@ public class ReportServlet extends HttpServlet {
     public String generatedUserID;
     @Override
 
-
-
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
@@ -27,16 +25,14 @@ public class ReportServlet extends HttpServlet {
         HttpSession session = request.getSession(false);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put( "serverResponse" , "Allowed" );
-        String textMsg = request.getParameter("textMsg");
-        LocalDate date = LocalDate.now();
-        LocalTime time = LocalTime.now();
 
-        System.out.println(textMsg);
-        System.out.println(date);
-        System.out.println(time);
-        AdminPost adminpost = new AdminPost(generatedUserID,textMsg,date,time);
-        adminpost.adminPost();
+        String id = request.getParameter("textMsg");
+        String type = request.getParameter("textMsg");
+        System.out.println(id);
+        System.out.println(type);
 
+        Report report = new Report(id,type);
+        report.report();
         out.write(jsonObject.toString());
         out.close();
     }
