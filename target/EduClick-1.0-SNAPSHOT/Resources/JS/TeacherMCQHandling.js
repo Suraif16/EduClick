f32579904a27e932f92daba860d39c71bbf7f3ac
Farzan <farzanroxz123@gmail.com>
2021-12-10 17:11:51 +0530

    added TeacherMCQHandling.js file, and added code to handle the next and previous buttons. and adjusted styles to handle div elements as buttons. and also removed the input button elements

---
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/TeacherMCQHandling.js
@@ -0,0 +1,14 @@
+const previousElement = document.getElementById( "previousQuestion" );
+const nextElement = document.getElementById( "nextQuestion" );
+
+previousElement.onclick = function (){
+
+    console.log( "clicked previous" );
+
+}
+
+nextElement.onclick = function (){
+
+    console.log( "clicked next" );
+
+}
