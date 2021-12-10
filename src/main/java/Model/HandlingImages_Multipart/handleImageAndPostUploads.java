c12a979b77e3512976b63cf923556aaa7f21d4e6
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2021-12-10 22:00:39 +0530

    add function for insert images

--- src/main/java/Model/HandlingImages_Multipart/handleImageAndPostUploads.java
+++ src/main/java/Model/HandlingImages_Multipart/handleImageAndPostUploads.java
@@ -1,6 +1,7 @@
 package Model.HandlingImages_Multipart;
 
 import Model.EducationalWork;
+import Model.NewsFeeds;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
@@ -58,6 +59,64 @@ public class handleImageAndPostUploads {
             EducationalWork educationalWork = new EducationalWork( message , type , localDate , localTime );
 
             return educationalWork.insertEducationalWork( imageFile , path , classroomId );
+
+
+        }catch ( Exception e ){
+
+            System.out.println( e );
+
+        }
+
+        return null;
+
+    }
+
+
+    /****************/
+
+    public static NewsFeeds uploadNewsFeedsImages(HttpServletRequest request , String path , LocalDate localDate , LocalTime localTime ){
+
+        String message = "";
+        ServletFileUpload servletFileUpload = new ServletFileUpload( new DiskFileItemFactory() );
+
+        try{
+
+            List<FileItem> files = servletFileUpload.parseRequest( request );
+
+            FileItem imageFile = null;
+
+            for (FileItem file : files ){
+
+                if ( file.isFormField() ){
+
+                    InputStream inputStream = file.getInputStream();
+                    byte[] bytes = new byte[ inputStream.available() ];
+                    inputStream.read( bytes );
+
+                    if ( file.getFieldName().equals( "message" ) ){
+
+                        message = new String( bytes );
+
+                    }/*else if ( file.getFieldName().equals( "type" ) ){
+
+                        type = new String( bytes );
+
+                    }*/
+
+                    inputStream.close();
+
+
+                }else{
+
+                    imageFile = file;
+
+                }
+
+            }
+
+            NewsFeeds newsFeeds = new NewsFeeds( message , localDate , localTime );
+
+            return newsFeeds.insertNewsFeeds( imageFile , path );
 
 
         }catch ( Exception e ){
