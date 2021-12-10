0cc966ae950309e0ff953cd7bcda97cb97387ad5
Rahuram <rahuram66@gmail.com>
2021-12-10 20:46:56 +0530

    making algorithem for ReportInsertDAO

--- src/main/java/DAO/ReportInsertDAO.java
+++ src/main/java/DAO/ReportInsertDAO.java
@@ -12,24 +12,56 @@ import java.util.ArrayList;
 
 public class ReportInsertDAO {
 
-    public String generatedSysPostUserId;
-    public String insert(AdminPost adminPost){
+
+    private static String generatedReportId;
+    private static String UserID;
+    private static String AnswerID;
+    private static String NF_postID;
+    private static String EpostID;
+
+
+    public static String insert(Report report){
         DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
         Connection connection = null;
 
         try {
             connection = dbConnectionPool.dataSource.getConnection();
-            String sql = "INSERT INTO Admin_Post_System_Updates (APTextMsg,APDate,APTime) VALUES (?,?,?)";
+            if(report.getType().equals("UserID")){
+                 UserID = report.getContentID();
+                 AnswerID = "";
+                 NF_postID = "";
+                 EpostID = "";
+            }else if(report.getType().equals("AnswerID")){
+                 UserID = "";
+                 AnswerID = report.getContentID();
+                 NF_postID = "";
+                 EpostID = "";
+            }else if(report.getType().equals("NF_postID")){
+                 UserID = "";
+                 AnswerID = "";
+                 NF_postID = report.getContentID();
+                 EpostID = "";
+            }else if(report.getType().equals("EpostID")){
+                 UserID = "";
+                 AnswerID = "";
+                 NF_postID = "";
+                 EpostID = report.getContentID();
+            }
+            int Count = 1;
+            String sql = "INSERT INTO Report (Count,UserID,AnswerID,NF_postID,EpostID) VALUES (?,?,?,?)";
             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            preparedStatement.setString(1, adminPost.getTextMsg());
-            preparedStatement.setString(2, String.valueOf(adminPost.getDate()));
-            preparedStatement.setString(3, String.valueOf(adminPost.getTime()));
+            preparedStatement.setString(1, String.valueOf(Count));
+            preparedStatement.setString(2, UserID);
+            preparedStatement.setString(3, AnswerID);
+            preparedStatement.setString(4, NF_postID);
+            preparedStatement.setString(5, EpostID);
+            //preparedStatement.setString(2, String.valueOf(report.getDate()));
 
             preparedStatement.execute();
             ResultSet resultSet = preparedStatement.getGeneratedKeys();
 
             if (resultSet.next()){
-                generatedSysPostUserId = resultSet.getString(1);
+                generatedReportId = resultSet.getString(1);
 
             }
             //returns userID
@@ -42,7 +74,7 @@ public class ReportInsertDAO {
         finally {
             if (connection != null) try { connection.close(); }catch (Exception ignore) {}
         }
-        return generatedSysPostUserId;
+        return generatedReportId;
     }
 
 }
