72ab14fd550d844fbf4928e962964b9f5326968d
Venushka <venushkachstc@gmail.com>
2021-12-12 12:01:53 +0530

    Added functions to get the data from the front end

--- src/main/webapp/Resources/JS/StudentEducationalPostsLoad.js
+++ src/main/webapp/Resources/JS/StudentEducationalPostsLoad.js
@@ -1,3 +1,6 @@
+const mimeTypeArray = [ "image/apng" , "image/avif" , "image/jpeg" , "image/png" , "image/webp" , "image/bmp" , "image/x-icon" , "image/tiff" , "image/heic" , "image/heif" ];
+
+
 const loadStudentEducationalPosts = function (){
     let httpreq = new XMLHttpRequest();
     httpreq.onreadystatechange = function (){
@@ -22,6 +25,8 @@ const loadStudentEducationalPosts = function (){
             window.location.replace("/EduClick_war_exploded/Login.html");
         }else if(jsonLoginResponse.serverResponse === "Allowed") {
             console.log("math me paththen awa bosa")
+
+            let postContents =  document.getElementById("postContents")
 
             /*let count = jsonLoginResponse.EPostContent.length - 1;
             for(i=0 ; i<=count ; i++){
@@ -92,7 +97,7 @@ const loadStudentEducationalPosts = function (){
                         '                                    <img id="attachmentImage" src="../Resources/Icons/outline_attach_file_white_24dp.png">' +
                         '                                    <input id="inputImage" type="file" accept="image/*">' +
                         '                                </label>' +
-                        '                                <input id="comment'+jsonLoginResponse.EPostContent+'" type="text" placeholder="   Comments...">' +
+                        '                                <input id="comment'+jsonLoginResponse.EPostContent[i].ePost.EPostID+'" type="text" placeholder="   Comments...">' +
                         '                                <input type="button" value="Post" class="answerShowButton">' +
                         '                                <input type="button" value="Delete Answer" class="answerShowButton">' +
                         '                            </div>' +
@@ -165,8 +170,8 @@ const loadStudentEducationalPosts = function (){
                         '                                    <img id="attachmentImage" src="../Resources/Icons/outline_attach_file_white_24dp.png">' +
                         '                                    <input id="inputImage" type="file" accept="image/*">' +
                         '                                </label>' +
-                        '                                <input id="comment" type="text" placeholder="   Comments...">' +
-                        '                                <input type="button" value="Post" class="answerShowButton" onclick="submitAnswers()">' +
+                        '                                <input id="comment'+jsonLoginResponse.EPostContent[i].ePost.EPostID+'" type="text" placeholder="   Comments...">' +
+                        '                                <input type="button" value="Post" class="answerShowButton" onclick="submitAnswers('+jsonLoginResponse.EPostContent[i].ePost.EPostID+')">' +
                         '                                <input type="button" value="Delete Answer" class="answerShowButton">' +
                         '                            </div>' +
                         '                        </div>' +
@@ -176,10 +181,8 @@ const loadStudentEducationalPosts = function (){
                         '        </div>'
                     postContents.innerHTML += htmlString;
                 }
+
             }
-
-
-
 
 
 
@@ -189,11 +192,120 @@ const loadStudentEducationalPosts = function (){
 
     }
 }
+const submitAnswers = function(EPostId){
 
-const submitAnswers = function(){
+    let comment = "comment"+EPostId;
     let images = document.getElementById( "inputImage" ).files;
-    let answers = document.getElementById("comment").value;
+    let answers = document.getElementById(comment).value;
 
     console.log("Answer is : "+answers);
-    // console.log(images)
+    console.log(images)
+
+    if ( answers === "" && images.length === 0 ){
+
+        console.log("empty")
+
+    }else{
+        console.log("Else nm hehehehehe")
+
+        let isAllImageValid = false;
+
+        for ( let i = 0; i < images.length; i++ ) {
+
+            isAllImageValid = isImageAccepted( images[i].type )
+            if ( !isAllImageValid ){
+                console.log("break");
+                break;
+
+            }
+
+        }
+
+        if ( isAllImageValid ){
+
+            let httpreq = new XMLHttpRequest();
+            let formData = new FormData();
+            httpreq.onreadystatechange = function(){
+
+                if ( this.readyState === 4 && this.status === 200 ){
+
+                    completeAnswersSend( this );
+
+
+                }
+
+            }
+
+            for ( let i = 0 ; i < images.length ; i++ ){
+
+                let x = "photo"+[i];
+                formData.append( x , images[i] );
+                console.log(i);
+
+            }
+
+            formData.append( "answers" , answers );
+            formData.append("ePostId",EPostId);
+            httpreq.open("POST","/EduClick_war_exploded/student/EducationalPostAnswersInsert" , true );
+            httpreq.send( formData );
+
+        }else{
+
+            console.log( "image type invalid" );
+
+        }
+
+    }
+    const completeAnswersSend = function ( httpreq ){
+        let jsonAnswerResponse = JSON.parse( httpreq.responseText );
+        if( jsonAnswerResponse.serverResponse === "null Session" || jsonAnswerResponse.serverResponse === "Not Allowed"){
+            window.location.replace("/EduClick_war_exploded/Login.html");
+        }else if(jsonAnswerResponse.serverResponse === "Allowed") {
+            console.log("Amo amo mamamehen awooooo");
+            console.log(jsonAnswerResponse.EPostAnswer)
+
+            let answersInPost = "answersInPost"+EPostId;
+            let answer = document.getElementById(answersInPost);
+            let now = new Date().getTime();
+            let extraTime = 7000;
+            while(new Date().getTime() < now + extraTime ){}
+            let htmlString =
+                '<div class="singleAnswer">' +
+                '                                    <div class="textAnswers" id="myComment">' +
+                                                        jsonAnswerResponse.EPostAnswer.answer +
+                '                                    </div>' +
+                '                                    <div class="pictureAnswers">' +
+                '                                        <a href="#">' +
+                '                                            <img src="../Resources/Images/AnswerImages/' + jsonAnswerResponse.EPostAnswer.imagePath + '.jpeg">' +
+                '                                        </a>' +
+                '                                    </div>' +
+                '                                    <div class="Marks">' +
+                                                        "Your marks is "+ jsonAnswerResponse.EPostAnswer.marks + "%"+
+                '                                    </div>' +
+                '                                </div>'
+            answer.innerHTML+=htmlString;
+
+        }
+        else{
+            alert("something went wrong!!!");
+        }
+    }
 }
+const isImageAccepted = function ( type ){
+
+    const arrayLength = mimeTypeArray.length;
+
+    for ( let i = 0; i < arrayLength; i++ ) {
+
+        if ( mimeTypeArray[i] === type ){
+
+            return true;
+
+        }
+
+    }
+
+    return false;
+
+}
+
