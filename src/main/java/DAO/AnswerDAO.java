7362621f1db5cc7d6340dfe0e57420772fa60416
Venushka <venushkachstc@gmail.com>
2021-12-14 12:22:53 +0530

    Added the DAO selection statement to get data

--- src/main/java/DAO/AnswerDAO.java
+++ src/main/java/DAO/AnswerDAO.java
@@ -2,6 +2,7 @@ package DAO;
 
 import Database.DBConnectionPool;
 import Model.Answers;
+import org.json.JSONObject;
 
 import java.sql.*;
 
@@ -40,5 +41,36 @@ public class AnswerDAO {
         System.out.println("Generated AnswerID is : "+generatedAnswerId);
 
         return generatedAnswerId;
+    }
+
+    public JSONObject selectAnswerDetails(String answerId){
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+        JSONObject jsonObject = new JSONObject();
+
+        try {
+            connection = dbConnectionPool.dataSource.getConnection();
+            String sql = "SELECT * FROM Answer WHERE AnswerID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1,answerId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                jsonObject.put("AnswerID",resultSet.getString("AnswerID"));
+                jsonObject.put("Date",resultSet.getString("Date"));
+                jsonObject.put("Time",resultSet.getString("Time"));
+                jsonObject.put("Marks",resultSet.getString("Marks"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (connection != null) try {
+                connection.close();
+            } catch (Exception ignore) {
+            }
+        }
+        return jsonObject;
     }
 }
