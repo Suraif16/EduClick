a73538c7faad476c3c312b8ceadc40b0f1e52153
Venushka <venushkachstc@gmail.com>
2021-12-18 09:21:15 +0530

    Added functions to save details in the student post answer relationship table

--- src/main/java/Controller/StudentMCQResultLoadServlet.java
+++ src/main/java/Controller/StudentMCQResultLoadServlet.java
@@ -2,11 +2,13 @@ package Controller;
 
 import Model.Answers;
 import Model.Question;
+import Model.User;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
@@ -21,6 +23,13 @@ public class StudentMCQResultLoadServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         response.setContentType("text/html");
+
+        HttpSession session = request.getSession( false );
+
+        User user = (User) session.getAttribute("User");
+
+        String userId = user.getUserId();
+
 
         JSONObject jsonObject = new JSONObject();
 
@@ -53,10 +62,12 @@ public class StudentMCQResultLoadServlet extends HttpServlet {
         }
         System.out.println("Your result is : "+result);
 
-        /*Answers answers = new Answers(LocalDate.now(),LocalTime.now(),result);
+        Answers answers = new Answers(LocalDate.now(),LocalTime.now(),result);
         String answerId = answers.enterMCQMarks();
         System.out.println("AnswerID is : "+answerId);
-        answers.saveMCQAnswers(answerId,studentAnswerList,postId);*/
+        answers.saveMCQAnswers(answerId,studentAnswerList,postId);
+        answers.saveMCQAnswerPostStudentRelationship(userId,answerId,postId);
+
 
         System.out.println("Everything inserted into tables smoothly");
 
