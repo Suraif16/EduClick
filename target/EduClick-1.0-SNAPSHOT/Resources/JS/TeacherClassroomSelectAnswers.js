be51047fe70653e0026a311d3cda31c2b778f5ca
Farzan <farzanroxz123@gmail.com>
2021-12-18 15:43:50 +0530

    selecting answers to single post at a time is complete

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/TeacherClassroomSelectAnswers.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/TeacherClassroomSelectAnswers.js
@@ -2,18 +2,89 @@ function showAnswers( id ){
 
     let answerId = "answersInPost" + id;
     let answerContainer = document.getElementById( answerId );
-    let com = document.getElementById("ans" + id) ;
 
     if (answerContainer.style.display === "none"){
 
+        getAnswersServer( id , answerContainer );
         answerContainer.style.display = "flex";
-
-        com.style.display = "flex";
 
     }else{
 
         answerContainer.style.display = "none";
-        com.style.display = "none"
+
+    }
+
+}
+
+const getAnswersServer = function ( id , answerContainer ){
+
+    let httpreq = new XMLHttpRequest();
+
+    httpreq.onreadystatechange = function (){
+
+        if ( this.readyState === 4 && this.status === 200 ){
+
+            complete( this )
+
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherClassroomSelectAnswersServlet" , true );
+    httpreq.setRequestHeader( "Content-type" , "application/x-www-form-urlencoded" );
+    httpreq.send( "epostId=" + id );
+
+    const complete = function ( httpreq ){
+
+        let jsonObject = JSON.parse( httpreq.responseText );
+        console.log( jsonObject );
+        let answerList = jsonObject.answerList;
+
+        answerContainer.innerHTML = "";
+
+        for ( let i = 0 ; i < answerList.length ; i++ ) {
+
+            let singleAnswer = '<div class="singleAnswer">' +
+                '<div class="answerUser">' +
+                '    <a href="/EduClick_war_exploded/userProfileRedirect?userId=' + answerList[i].userId + '" class="answerProfile">' +
+                '        <div class="answerProfileImage">' +
+                '            <img class="answerProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg">' +
+                '        </div>' +
+                '        <div class="answerProfileName" >' + answerList[i].studentName + '</div>' +
+                '    </a>' +
+                '    <div class="postTimeAndDate">' +
+                answerList[i].answerTime + ' | ' + answerList[i].answerDate +
+                '    </div>' +
+                '    <div class="userOptions">' +
+                '        <input class="userOptionsButton" type="button" value="    " id="answerOption1" onclick="showOptionMenu(1,\'answerOption\')">' +
+                '    </div>' +
+                '</div>' +
+                '<div class="textAnswers">' +
+
+                answerList[i].content +
+
+                '</div>';
+
+            if ( answerList[i].imageStatus === "true" ){
+
+                singleAnswer +=    '<div class="pictureAnswers">' +
+                    '    <a href="#">' +
+                    '        <img src="../Resources/Images/AnswerImages/' + answerList[i].answerId + '.jpeg">' +
+                    '    </a>' +
+                    '</div>';
+
+            }
+
+            singleAnswer +=    '<div class="marksForAnswers">' +
+                '    <input type="range" value="0" max="100" oninput="answer' + answerList[i].answerId + '.value = this.value" class="marksForAnswersRange">' +
+                '    <output id="answer' + answerList[i].answerId + '" class="marksForAnswersRangeValue">0</output>' +
+                '</div>' +
+                '</div>';
+
+            answerContainer.innerHTML += singleAnswer;
+
+        }
+
     }
 
 }
