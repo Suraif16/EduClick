62e6e80ccf24dc8c4c0e51af4b8d0f9db42b78bf
Rahuram <rahuram66@gmail.com>
2022-01-15 17:39:51 +0530

    creating AdminDatacheckDAO.java

---
+++ src/main/java/DAO/AdminDatacheckDAO.java
@@ -0,0 +1,72 @@
+package DAO;
+
+import Database.DBConnectionPool;
+import Model.AdminDatacheck;
+import Model.User;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AdminDatacheckDAO<teacherArrayList> {
+
+
+    public User select(User user) {
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+        String userType = "";
+        try {
+            connection = dbConnectionPool.dataSource.getConnection();
+            String sql = "select FirstName, LastName, ProfilePic, DOB, MobileNum, UserType, Gender, Country, City from Users where UserID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, user.getUserId());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String firstName = resultSet.getString("FirstName");
+                String lastName = resultSet.getString("LastName");
+                String dataOfBirth = resultSet.getString("DOB");
+                String mobileNumber = resultSet.getString("MobileNum");
+                String profilePicture = resultSet.getString("ProfilePic");
+                String country = resultSet.getString("Country");
+                String city = resultSet.getString("City");
+                String gender = resultSet.getString("Gender");
+                userType = resultSet.getString("UserType");
+
+                user.setFirstName(firstName);
+                user.setLastName(lastName);
+                user.setDateOfBirth(LocalDate.parse(dataOfBirth));
+                user.setMobileNumber(mobileNumber);
+                user.setProfilePicture(profilePicture);
+                user.setCountry(country);
+                user.setCity(city);
+                user.setGender(gender);
+                user.setUserType(userType);
+
+            }
+            resultSet.close();
+            preparedStatement.close();
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            if (connection != null) try {
+                connection.close();
+            } catch (Exception ignore) {
+            }
+        }
+        return user;
+    }
+
+
+
+
+
+
+}
+
+
