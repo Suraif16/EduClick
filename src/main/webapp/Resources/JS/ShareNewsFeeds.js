09310608ddeeec956559416be1ba600682d71218
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2022-03-17 11:55:02 +0530

    edit js code

--- src/main/webapp/Resources/JS/ShareNewsFeeds.js
+++ src/main/webapp/Resources/JS/ShareNewsFeeds.js
@@ -3,7 +3,7 @@ function shareNewsFeeds ( NewsFeedspostID){
     console.log(NewsFeedspostID);
 
     let id = NewsFeedspostID;
-    console.log(id+" = sharedID");
+    console.log(id+" = sharedID ****");
 
     let httpreq = new XMLHttpRequest();
 
@@ -21,7 +21,7 @@ function shareNewsFeeds ( NewsFeedspostID){
 
     function completeShare(httpreq){
 
-        let jsonResponse = JSON.parse( httpreq.responseText );
+   /*     let jsonResponse = JSON.parse( httpreq.responseText );
 
         if( jsonResponse.serverResponse === "null Session" || jsonResponse.serverResponse === "Not Allowed"){
             window.location.replace("/EduClick_war_exploded/Login.html");
@@ -93,9 +93,87 @@ function shareNewsFeeds ( NewsFeedspostID){
 
             ppostContents.innerHTML += innerPreviouseHTML;
 
-        }else{
-            alert("something went wrong!!!");
-        }
-    }
+*/
+        let jsonLoginResponse = JSON.parse(httpreq.responseText);
 
-}
+        if (jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed") {
+            window.location.replace("/EduClick_war_exploded/Login.html");
+        } else if (jsonLoginResponse.serverResponse === "Allowed") {
+
+            console.log("$$$$$$");
+
+            let k = jsonLoginResponse.ImagePath.length;
+            for (i = 0; i < k; i++) {
+                console.log(jsonLoginResponse.ImagePath[i]);
+            }
+
+            const postContents = document.getElementById("postContents");
+            postContents.innerHTML = "";
+
+            let count = jsonLoginResponse.NewsFeedID.length;
+            for (i = 0; i < count; i++) {
+
+                let htmlString =
+                    '        <div class="post">    ' +
+                    '              <div class="postCon tentContainer">' +
+                    '                   <div class="postProfileSection">' +
+                    '                        <a href="TeacherProfile.html" class="postProfile">' +
+                    '                           <div class="postProfileImage">' +
+                    '                               <img class="postProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg"> ' +
+                    '                           </div>' +
+                    '                           <div class="postProfileName" >' + jsonLoginResponse.TeacherName[i].firstName + " " +
+                    jsonLoginResponse.TeacherName[i].lastName +
+                    '</div>' +
+                    '                           <div class="postTimeAndDate" >' +
+                    jsonLoginResponse.NewsFeedsDetails[i].Time + '|' +
+                    jsonLoginResponse.NewsFeedsDetails[i].Date +
+                    '                            </div>' +
+                    '                        </a>' +
+                    '                   </div>' +
+                    '               </div>' +
+                    '               <div class="postContentContainer">' +
+                    '                   <div class="postData">' +
+                    '                       <div class="postMessage">' + jsonLoginResponse.NewsFeedsDetails[i].Caption +
+
+                    '                       </div>' +
+                    '                       <div class="postPicture">' +
+                    '                           <div class="postPictureImageContainer">' +
+                    '                               <img class="postPictureImage" src="../Resources/Images/seminar-text.jpg >' + jsonResponse.NewsFeed.imagePath + '.jpeg">' +
+                    '                           </div>' +
+                    '                       </div>' +
+                    '                     </div>' +
+                    '                  </div>' +
+                    '                   <div class="postContentContainer">' +
+                    '                       <div class="postLikeShareButtons">' +
+                    '                           <div class="likeShareButtons" >' +
+                    '                               <input type="button" value="Like" class="like">' +
+                    '                           </div>' +
+                    '                           <div class="countOfLikeShare" >' +
+                    '                               <div class="likeCount">' +
+                    '                                   2793 Likes' +
+                    '                               </div >' +
+                    '                               <div class="emptySpaceLikeShare">' +
+                    '                               </div>' +
+                    '                               <div class="shareCount">' +
+                    '                                   18198 Shares' +
+                    '                               </div>' +
+                    '                              </div>' +
+                    '                           <div class="likeShareButtons" >' +
+                    '                               <input type="button" value="Share" class="share">' +
+                    '                           </div>' +
+                    '                        </div>' +
+                    '                       </div>' +
+                    '                   </div>'
+
+
+                postContents.innerHTML += htmlString;
+            }
+
+        }
+
+        else
+            {
+                alert("something went wrong!!!");
+            }
+        }}
+
