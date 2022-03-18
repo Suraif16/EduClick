459d0da6665b022585cb24d826fd684804b0c3b2
Rahuram <rahuram66@gmail.com>
2022-03-18 23:32:45 +0530

    change mistakes

--- src/main/webapp/Resources/JS/AdminDatacheckteacher.js
+++ src/main/webapp/Resources/JS/AdminDatacheckteacher.js
@@ -1,55 +1,92 @@
-
-
 function checkdata(){
+    searchFunction();
+}
 
-    getServerData();
+const searchFunction = function ( ) {
+    let searchType = "Teacher";
+    const userName = document.getElementById( "comment" ).value
+    console.log(userName);
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+        if ( httpreq.readyState === 4 && httpreq.status === 200){
+            displayTeacher( this );
+        }
+    }
+
+    httpreq.open("POST", "/EduClick_war_exploded/AdminDatacheckServlet" ,true);
+    httpreq.setRequestHeader("Content-type" , "application/x-www-form-urlencoded");
+    httpreq.send("userName=" + userName + "&searchType=" + searchType );
+
+
+    const displayTeacher = function ( httpreq ){
+        let jsonLoginResponse = JSON.parse( httpreq.responseText );
+        const postContents = document.getElementById( "postContents" );
+        postContents.innerHTML = "";
+
+        for ( i = 0 ; i < jsonLoginResponse.searchResult.length ; i++ ){
+
+            let post = '<div class="post">'+
+                '<div class="postContentContainer">'+
+                '<div class="postProfileSection">'+
+                '<a href="#" class="postProfile">'+
+                '<div class="postProfileImage">'+
+                '<img class="postProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg">'+
+                '</div>'+
+                '<div class="postProfileName">'+jsonLoginResponse.searchResult[i].firstName+" "+jsonLoginResponse.searchResult[i].lastName+
+                '</div>'+
+                '</a>'+
+                '</div>'+
+                '</div>'+
+
+                '<div class="postContentContainer">'+
+                '<div class="postData">'+
+                '<div class="postMessage">'+
+                'UserId'+
+                '</div>'+
+                '<div class="postMessage" >'+jsonLoginResponse.searchResult[i].userId+'</div>'+
+
+                '<div class="postMessage">'+
+                'FirstName'+
+                '</div>'+
+                '<div class="postMessage" >'+jsonLoginResponse.searchResult[i].firstName+'</div>'+
+
+                '<div class="postMessage">'+
+                'LastName'+
+                '</div>'+
+                '<div class="postMessage" >'+jsonLoginResponse.searchResult[i].lastName+'</div>'+
+
+                '<div class="postMessage">'+
+                'Gender'+
+                '</div>'+
+                '<div class="postMessage" >'+jsonLoginResponse.searchResult[i].gender+'</div>'+
+
+                '<div class="postMessage">'+
+                'DateofBirth'+
+                '</div>'+
+                '<div class="postMessage" >'+jsonLoginResponse.searchResult[i].dataOfBirth+'</div>'+
+
+                '<div class="postMessage">'+
+                'Country'+
+                '</div>'+
+                '<div class="postMessage" >'+jsonLoginResponse.searchResult[i].country+'</div>'+
+
+                '<div class="postMessage">'+
+                'RegistrationDate'+
+                '</div>'+
+                '<div class="postMessage" >'+jsonLoginResponse.searchResult[i].dataOfReg+'</div>'+
+
+                '<div class="postMessage">'+
+                'RegistrationTime'+
+                '</div>'+
+                '<div class="postMessage" >'+jsonLoginResponse.searchResult[i].timeOfReg+'</div>'+
+                '</div>'+
+                '</div>'+
+                '</div>';
+            postContents.innerHTML += post;
+        }
+    }
 
 }
 
-const getServerData = function (){
-    let search = document.getElementById( "comment" ).value;
-    console.log(search);
-
-    let httpReq = new XMLHttpRequest();
-    httpReq.onreadystatechange = function () {
-
-        if (this.readyState === 4 && this.status === 200) {
-            completegetdata(this)
-        }
-
-    }
-    httpReq.open( "GET" , "/EduClick_war_exploded/AdminDatacheckServlet" , true);
-    httpReq.send();
-    function completegetdata( httpreq ) {
-        let jsonLoginResponse = JSON.parse(httpreq.responseText);
-        if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
-            window.location.replace("/EduClick_war_exploded/Login.html");
-        }else if(jsonLoginResponse.serverResponse === "Allowed") {
-            if(jsonLoginResponse.getUserType === "Teacher"){
-                if(jsonLoginResponse.getFirstName === search || jsonLoginResponse.getLastName === search ){
-                    document.getElementById("getUserId").innerHTML = jsonLoginResponse.getUserId;
-                    console.log(jsonLoginResponse.getUserId);
-                    document.getElementById("getFirstName").innerHTML = jsonLoginResponse.getFirstName;
-                    console.log(jsonLoginResponse.getFirstName);
-                    document.getElementById("getLastName").innerHTML = jsonLoginResponse.getLastName;
-                    console.log(jsonLoginResponse.getLastName);
-                    document.getElementById("getGender").innerHTML = jsonLoginResponse.getGender;
-                    console.log(jsonLoginResponse.getGender);
-                    document.getElementById("getDateOfBirth").innerHTML = jsonLoginResponse.getDateOfBirth;
-                    console.log(jsonLoginResponse.getDateOfBirth);
-                    document.getElementById("getCountry").innerHTML = jsonLoginResponse.getCountry;
-                    console.log(jsonLoginResponse.getCountry);
-                    document.getElementById("getRegistrationDate").innerHTML = jsonLoginResponse.getRegistrationDate;
-                    console.log(jsonLoginResponse.getRegistrationDate);
-                    document.getElementById("getRegistrationTime").innerHTML = jsonLoginResponse.getRegistrationTime;
-                    console.log(jsonLoginResponse.getRegistrationTime);
-                }
-            }
-
-        }else{
-            alert("something went wrong!!!");
-        }
-    }
-}
 
 
