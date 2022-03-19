8ec56d96a0bb732090b1e77bb6260d1860422c7b
Venushka <venushkachstc@gmail.com>
2022-03-19 17:09:42 +0530

    Added new functionalitis to displa teachers list correct

--- src/main/java/DAO/EnrollRequestDAO.java
+++ src/main/java/DAO/EnrollRequestDAO.java
@@ -135,4 +135,97 @@ public class EnrollRequestDAO {
 
 
     }
+
+    public ArrayList<String> requestCheck(String classroomId){
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+        ArrayList<String> studentList = new ArrayList<>();
+
+        try {
+            connection = dbConnectionPool.dataSource.getConnection();
+            String sql = "SELECT From_UserID FROM Enroll_Request WHERE To_ClassroomID = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+
+            preparedStatement.setString(1,classroomId);
+
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while ( resultSet.next() ){
+               studentList.add(resultSet.getString( "From_UserID" ));
+
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        finally {
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
+        }
+        return studentList;
+
+
+    }
+
+    public String deleteRequest(String classroomId,String userId){
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+
+        try {
+            connection = dbConnectionPool.dataSource.getConnection();
+
+            String sql =  "DELETE FROM Enroll_Request WHERE From_UserID = ? AND To_ClassroomID = ? ";
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+
+            preparedStatement.setString(1,userId);
+            preparedStatement.setString(2,classroomId);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
+        }
+        return "Request Deleted";
+    }
+
+    public ArrayList<String> getRequestedClassroomList(String userId){
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+        ArrayList<String> requestedClassroomList = new ArrayList<>();
+
+        try {
+            connection = dbConnectionPool.dataSource.getConnection();
+            String sql = "SELECT To_ClassroomID FROM Enroll_Request WHERE From_UserID = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+
+            preparedStatement.setString(1,userId);
+
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while ( resultSet.next() ){
+                requestedClassroomList.add(resultSet.getString( "To_ClassroomID" ));
+
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        finally {
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
+        }
+        return requestedClassroomList;
+    }
 }
