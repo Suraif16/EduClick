b7abbbe93d41561f9592d9e3ad784ca3c3f088cf
Farzan <farzanroxz123@gmail.com>
2022-03-20 14:33:08 +0530

    completed the friend request cancel component

--- src/main/java/Controller/Requests/cancelFriendRequestServlet.java
+++ src/main/java/Controller/Requests/cancelFriendRequestServlet.java
@@ -1,25 +1,33 @@
 package Controller.Requests;
 
 import Model.Requests;
-import Model.User;
+import org.json.JSONObject;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class cancelFriendRequestServlet extends HttpServlet {
 
     @Override
-    public void doPost(HttpServletRequest request , HttpServletResponse response ){
+    public void doPost(HttpServletRequest request , HttpServletResponse response ) throws IOException {
 
-        HttpSession session = request.getSession( false );
-        User user = ( User ) session.getAttribute( "User" );
-        String fromID = user.getUserId();
-        String toID = request.getParameter( "toID" );
+        PrintWriter out = response.getWriter();
+        response.setContentType( "text/html" );
+
+        String fromID = request.getParameter("fromId");
+        String toID = request.getParameter( "toId" );
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put( "serverResponse"  , "Allowed" );
 
         Requests requests = new Requests( fromID , toID );
         requests.cancelRequest();
+
+        out.write( jsonObject.toString() );
+        out.close();
 
     }
 
--- target/EduClick-1.0-SNAPSHOT/Resources/JS/Notification.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/Notification.js
@@ -143,6 +143,33 @@ function FriendRequestAccept( fromId , toId ){
 function FriendRequestDecline(fromId , toId ){
 
     console.log( "friend request Decline" , fromId , toId );
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function(){
+
+        if ( this.readyState === 4 && this.status === 200){
+
+            let jsonResponse = JSON.parse( this.responseText );
+
+            if( jsonResponse.serverResponse === "null Session" || jsonResponse.serverResponse === "Not Allowed"){
+                window.location.replace("/EduClick_war_exploded/Login.html");
+            }else if(jsonResponse.serverResponse === "Allowed") {
+                /* This is where I need work everytime as per the authentication filter*/
+                const singleNotificaiton = document.getElementById( "Friend" + fromId + "" + toId );
+
+                singleNotificaiton.style.display = "none";
+                declineFriendRequestActivityStatus();
+
+            }else{
+                alert("something went wrong!!!");
+            }
+
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/user/cancelFriendRequest" , true );
+    httpreq.setRequestHeader("Content-type" , "application/x-www-form-urlencoded");
+    httpreq.send( "fromId=" + fromId + "&toId=" + toId );
 
 }
 
@@ -164,11 +191,6 @@ function EnrollRequestAccept( fromId , toId ){
                 const singleNotificaiton = document.getElementById( "Enroll" + fromId + "" + toId );
 
                 if ( jsonResponse.enrollStatus === true ){
-                    /*console.log( "buttonsEnroll" + fromId + "" + toId );console.log("please work");
-                    document.getElementById( "buttonsEnroll" + fromId + "" + toId ).style.backgroundColor = "red";
-                    let now = new Date().getTime();
-                    let extraTime = 2500;
-                    while(new Date().getTime() < now + extraTime ){}*/
 
                     singleNotificaiton.style.display = "none";
                     acceptEnrollRequestActivityStatus();
