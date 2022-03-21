a48e0bff6a1c8c846d8a5568a4c31c4e28465a84
Rahuram <rahuram66@gmail.com>
2022-03-21 14:18:33 +0530

    set the upload epost images

--- src/main/java/Model/HandlingImages_Multipart/handleImageAndPostUploads.java
+++ src/main/java/Model/HandlingImages_Multipart/handleImageAndPostUploads.java
@@ -270,5 +270,35 @@ public class handleImageAndPostUploads {
 
     }
 
+    public static AdminWork uploadEPostImages(HttpServletRequest request, String path, LocalDate localDate, LocalTime localTime ) {
+        String message = "";
+        ServletFileUpload servletFileUpload = new ServletFileUpload(new DiskFileItemFactory());
+
+        try {
+            List<FileItem> files = servletFileUpload.parseRequest(request);
+            FileItem imageFile = null;
+            for (FileItem file : files) {
+                if (file.isFormField()) {
+                    InputStream inputStream = file.getInputStream();
+                    byte[] bytes = new byte[inputStream.available()];
+                    inputStream.read(bytes);
+                    if ( file.getFieldName().equals("message") ) {
+                        message = new String(bytes);
+                    }
+                    inputStream.close();
+                } else {
+                    imageFile = file;
+                }
+            }
+
+            AdminWork adminWork = new AdminWork( message ,  localDate , localTime );
+            return adminWork.insertAdminPostWork( imageFile , path );
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
 }
