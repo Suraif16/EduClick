832b568aa6cb81811ff82cf8d80a507e40ee84be
Rahuram <rahuram66@gmail.com>
2022-03-22 10:58:31 +0530

    in put if condtion for normal post and image post

--- src/main/webapp/Resources/JS/AdminPost.js
+++ src/main/webapp/Resources/JS/AdminPost.js
@@ -24,39 +24,59 @@ const getServerData = function (){
         let jsonResponse = JSON.parse(httpreq.responseText);
         const postContents = document.getElementById("request");
         postContents.innerHTML = "";
-
+        const one = "1";
         let count = jsonResponse.AdminPostDetails.length;
         for( i=0; i< count; i++ ){
-
-            let htmlString ='<div class="post">' +
-                '<div class="postContentContainer">'+
-                '<div class="postProfileSection">'+
-                '<a href="#" class="postProfile">'+
-                '<div class="postProfileImage">'+
-                '<img class="postProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg">'+
-                '</div>'+
-                '<div class="postProfileName" >Admin</div>'+
-                '<div class="postTimeAndDate">'+jsonResponse.AdminPostDetails[i].date+'|'+ jsonResponse.AdminPostDetails[i].time+'</div>'+
-                '</a>'+
-                '</div>'+
-                '</div>'+
-                '<div class="postContentContainer">'+
-                '<div class="postData">'+
-                '<div class="postMessage">'+
-                jsonResponse.AdminPostDetails[i].caption+
-                '</div>'+
-                '<div class="postPicture">' +
-                '<div class="postPictureImageContainer">' +
-                '<img class="postPictureImage" src="../Resources/Images/AdminPostImages/' + jsonResponse.AdminPostDetails[i].aPId + '.jpeg">' +
-                '</div>'+
-                '</div>'+
-                '</div>'+
-                '</div>'+
-                '</div>';
-            postContents.innerHTML += htmlString;
-
+            if(jsonResponse.AdminPostDetails[i].status === one){
+                let htmlString ='<div class="post">' +
+                    '<div class="postContentContainer">'+
+                    '<div class="postProfileSection">'+
+                    '<a href="#" class="postProfile">'+
+                    '<div class="postProfileImage">'+
+                    '<img class="postProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg">'+
+                    '</div>'+
+                    '<div class="postProfileName" >Admin</div>'+
+                    '<div class="postTimeAndDate">'+jsonResponse.AdminPostDetails[i].date+'|'+ jsonResponse.AdminPostDetails[i].time+'</div>'+
+                    '</a>'+
+                    '</div>'+
+                    '</div>'+
+                    '<div class="postContentContainer">'+
+                    '<div class="postData">'+
+                    '<div class="postMessage">'+
+                    jsonResponse.AdminPostDetails[i].caption+
+                    '</div>'+
+                    '<div class="postPicture">' +
+                    '<div class="postPictureImageContainer">' +
+                    '<img class="postPictureImage" src="../Resources/Images/AdminPostImages/' + jsonResponse.AdminPostDetails[i].aPId + '.jpeg">' +
+                    '</div>'+
+                    '</div>'+
+                    '</div>'+
+                    '</div>'+
+                    '</div>';
+                postContents.innerHTML += htmlString;
+            }else{
+                let htmlString ='<div class="post">' +
+                    '<div class="postContentContainer">'+
+                    '<div class="postProfileSection">'+
+                    '<a href="#" class="postProfile">'+
+                    '<div class="postProfileImage">'+
+                    '<img class="postProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg">'+
+                    '</div>'+
+                    '<div class="postProfileName" >Admin</div>'+
+                    '<div class="postTimeAndDate">'+jsonResponse.AdminPostDetails[i].date+'|'+ jsonResponse.AdminPostDetails[i].time+'</div>'+
+                    '</a>'+
+                    '</div>'+
+                    '</div>'+
+                    '<div class="postContentContainer">'+
+                    '<div class="postData">'+
+                    '<div class="postMessage">'+
+                    jsonResponse.AdminPostDetails[i].caption+
+                    '</div>'+
+                    '</div>'+
+                    '</div>'+
+                    '</div>';
+                postContents.innerHTML += htmlString;
+            }
         }
-
     }
-
 }
