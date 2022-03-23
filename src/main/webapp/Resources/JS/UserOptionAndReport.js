74d8947f41a81b8cbc3293684e87f13f59ae1582
Rahuram <rahuram66@gmail.com>
2022-03-23 17:28:58 +0530

    toke errors

--- src/main/webapp/Resources/JS/UserOptionAndReport.js
+++ src/main/webapp/Resources/JS/UserOptionAndReport.js
@@ -44,7 +44,7 @@ const report = function ( id , type ){
     let httpreq = new XMLHttpRequest();
     httpreq.onreadystatechange = function (){
         if (this.readyState === 4 && this.status === 200){
-            completeLogin( this ) /*This is where we get the response when the request was successfully sent and a successfully response is received */
+            //completeLogin( this ) /*This is where we get the response when the request was successfully sent and a successfully response is received */
         }
     }
 
