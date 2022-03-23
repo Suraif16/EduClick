eb5e2462a86d01d0ac538ce5e42ff4191b3127b8
Venushka <venushkachstc@gmail.com>
2022-03-24 00:02:21 +0530

    Added a new query to delete classroom

--- src/main/java/DAO/EnrollDAO.java
+++ src/main/java/DAO/EnrollDAO.java
@@ -427,6 +427,30 @@ public class EnrollDAO {
         
     }
 
+    public void unenrollFromClassroom(String userId,String classroomId){
+
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+
+        try {
+            connection = dbConnectionPool.dataSource.getConnection();
+
+            String sql =  "DELETE FROM Enroll WHERE UserID = ? AND ClassroomID = ? ";
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+
+            preparedStatement.setString(1,userId);
+            preparedStatement.setString(2,classroomId);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
+        }
+
+    }
+
 
 
 
