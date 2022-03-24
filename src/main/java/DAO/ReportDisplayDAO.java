4b650bfc2a3f89fb1746e100aecb0146cbed77f5
Rahuram <rahuram66@gmail.com>
2022-03-24 11:47:03 +0530

    select report details

--- src/main/java/DAO/ReportDisplayDAO.java
+++ src/main/java/DAO/ReportDisplayDAO.java
@@ -1,7 +1,7 @@
 package DAO;
 
 import Database.DBConnectionPool;
-import Model.Admin;
+import Model.Report;
 import Model.User;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -12,58 +12,41 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class ReportDAO {
+public class ReportDisplayDAO {
 
 
-    public Admin selectreport(Admin admin) {
+    public JSONArray getAdminPostDetails() {
         DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
         Connection connection = null;
+        JSONArray jsonArray = new JSONArray();
 
-
-        Date date = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String strDate = formatter.format(date);
-        System.out.println("Date Format with MM/dd/yyyy : " + strDate);
         try {
             connection = dbConnectionPool.dataSource.getConnection();
-            String sql = "select ReportID,Count,UserID,NF_postID,EpostID FROM Report";
+
+            String sql = "select ReportID,Count,Report_delete_flag,UserID,AnswerID,NF_postID,EpostID FROM Report";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
                 String ReportID = resultSet.getString("ReportID");
                 String Count = resultSet.getString("Count");
+                String Report_delete_flag = resultSet.getString("Report_delete_flag");
                 String UserID = resultSet.getString("UserID");
+                String AnswerID = resultSet.getString("AnswerID");
                 String NF_postID = resultSet.getString("NF_postID");
                 String EpostID = resultSet.getString("EpostID");
 
-                if (NF_postID.equals("NULL")&&EpostID.equals("NULL")) {
-                    //accont
-                    JSONObject jsonObject = new JSONObject();
-                    jsonObject.put("ReportID",ReportID);
-                    jsonObject.put("UserID",UserID);
-                    jsonObject.put("Count",Count);
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("aPId",ReportID);
+                jsonObject.put("caption",Count);
+                jsonObject.put("date",Report_delete_flag);
+                jsonObject.put("time",UserID);
+                jsonObject.put("status",AnswerID);
+                jsonObject.put("status",NF_postID);
+                jsonObject.put("status",EpostID);
 
-                } else if (UserID.equals("NULL")&&EpostID.equals("NULL")) {
-
-                    //Newsfeedpost
-                    JSONObject jsonObject = new JSONObject();
-                    jsonObject.put("ReportID",ReportID);
-                    jsonObject.put("NF_postID",NF_postID);
-                    jsonObject.put("Count",Count);
-                }else if (UserID.equals("NULL")&&NF_postID.equals("NULL")) {
-                    //post
-                    JSONObject jsonObject = new JSONObject();
-                    jsonObject.put("ReportID",ReportID);
-                    jsonObject.put("EpostID",EpostID);
-                    jsonObject.put("Count",Count);
-
-                }
+                jsonArray.put(jsonObject);
             }
-
-
-            resultSet.close();
-            preparedStatement.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -71,13 +54,11 @@ public class ReportDAO {
             if (connection != null) try {
                 connection.close();
             } catch (Exception ignore) {
+                ignore.printStackTrace();
             }
         }
-
-        return admin;
+        return jsonArray;
     }
-
-
 }
 
 
