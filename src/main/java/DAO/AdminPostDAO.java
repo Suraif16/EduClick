479d2de58e72a9fe5b20dc3ac291c772a0a4d213
Rahuram <rahuram66@gmail.com>
2022-03-24 11:04:34 +0530

    making delete funtion for admin post

--- src/main/java/DAO/AdminPostDAO.java
+++ src/main/java/DAO/AdminPostDAO.java
@@ -124,4 +124,27 @@ public class AdminPostDAO {
         return jsonArray;
     }
 
+    public String deleteRecord(String userId){
+        DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
+        Connection connection = null;
+
+        try {
+            connection = dbConnectionPool.dataSource.getConnection();
+
+            String sql =  "DELETE FROM Admin_Post_System_Updates WHERE SysPostID = ? ";
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+
+            preparedStatement.setString(1,userId);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (connection != null) try { connection.close(); }catch (Exception ignore) {}
+        }
+        return "Adminpost Deleted";
+
+    }
+
 }
