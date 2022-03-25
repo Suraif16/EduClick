f98c394deaad1f2bcbc7f399764d44cb13ed9297
Rahuram <rahuram66@gmail.com>
2022-03-25 12:27:43 +0530

    creating servlet for admin delete reported posts

---
+++ src/main/java/Controller/AdminDeleteReportPostServlet.java
@@ -0,0 +1,31 @@
+package Controller;
+
+
+import Model.AdminWork;
+import org.json.JSONObject;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AdminDeleteReportPostServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+
+        JSONObject jsonObject = new JSONObject();
+        HttpSession session = request.getSession( false );
+
+        String id = request.getParameter("id");
+        System.out.println("delete value " + id);
+        AdminWork delete = new AdminWork();
+        delete.adminDelete(id);
+        out.write(jsonObject.toString());
+        out.close();
+    }
+
+}
