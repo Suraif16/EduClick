8e1217a36032223b2dc61defad94dc6be6e17f26
Rahuram <rahuram66@gmail.com>
2022-03-25 10:22:35 +0530

    making logic for display reported post

--- src/main/java/DAO/ReportDisplayDAO.java
+++ src/main/java/DAO/ReportDisplayDAO.java
@@ -21,10 +21,14 @@ import java.sql.SQLException;
 
 public class ReportDisplayDAO {
 
+    public JSONArray getAdminPostDetails(  ){
 
-    public JSONArray getAdminPostDetails() {
+
         DBConnectionPool dbConnectionPool = DBConnectionPool.getInstance();
         Connection connection = null;
+
+        JSONArray jsonArray = new JSONArray();
+
         PreparedStatement preparedStatement = null;
         PreparedStatement preparedStatement1 = null;
         PreparedStatement preparedStatement2 = null;
@@ -33,71 +37,82 @@ public class ReportDisplayDAO {
         ResultSet resultSet1 = null;
         ResultSet resultSet2 = null;
 
-        JSONArray jsonArray = new JSONArray();
+        try{
 
-        try {
             connection = dbConnectionPool.dataSource.getConnection();
+            connection.setAutoCommit( false );
 
             String sql = "select Count,EpostID FROM Report";
-            preparedStatement = connection.prepareStatement(sql);
-            resultSet = preparedStatement.executeQuery();
+            preparedStatement = connection.prepareStatement( sql );
 
-            while (resultSet.next()) {
+
+            String sql1 = "SELECT NFPostID,Date, Time, Caption,ImageStatus FROM NewsFeeds ";
+            preparedStatement1 = connection.prepareStatement( sql1 );
+
+            String sql2 = "SELECT ImagePath,NFPostID FROM News_Feed_Image ";
+            preparedStatement2 = connection.prepareStatement( sql2 );
+
+            resultSet = preparedStatement.executeQuery();
+            System.out.println("dao");
+            while( resultSet.next() ){
                 String Count = resultSet.getString("Count");
                 String EpostID = resultSet.getString("EpostID");
-
-                while (resultSet1.next()) {
-                    String sql1 = "SELECT NFPostID,Date, Time, Caption FROM NewsFeeds ";
-                    preparedStatement1 = connection.prepareStatement(sql1);
-                    resultSet = preparedStatement.executeQuery();
-                    String NFPostID = resultSet.getString("NFPostID");
-
+                System.out.println("EpostID " +EpostID);
+                System.out.println("Count " +Count);
+                resultSet1 = preparedStatement1.executeQuery();
+                while ( resultSet1.next() ){
+                    String NFPostID = resultSet1.getString("NFPostID");
                     if(EpostID==NFPostID){
-                    String Date = resultSet.getString("Date");
-                    String Time = resultSet.getString("Time");
-                    String Caption = resultSet.getString("Caption");
-                        String sql2 = "SELECT ImagePath,NFPostID FROM News_Feed_Image ";
-                        preparedStatement2 = connection.prepareStatement(sql2);
-                        resultSet2 = preparedStatement.executeQuery();
-                        while (resultSet2.next()){
+                        System.out.println("NFPostID "+NFPostID);
+                        String Date = resultSet1.getString("Date");
+                        String Time = resultSet1.getString("Time");
+                        String Caption = resultSet1.getString("Caption");
+                        String ImageStatus = resultSet1.getString("ImageStatus");
+                        resultSet2 = preparedStatement2.executeQuery();
+                        while ( resultSet2.next() ){
                             String ANFPostID = resultSet2.getString("NFPostID");
-                            if(NFPostID==ANFPostID){
+                            if(ANFPostID==NFPostID){
+                                System.out.println("ANFPostID "+ANFPostID);
                                 String ImagePath = resultSet2.getString("ImagePath");
+                                System.out.println("ImagePath "+ImagePath);
                                 JSONObject jsonObject = new JSONObject();
                                 jsonObject.put("count",Count);
                                 jsonObject.put("nFPostID",NFPostID);
                                 jsonObject.put("date",Date);
                                 jsonObject.put("time",Time);
                                 jsonObject.put("caption",Caption);
-                                jsonObject.put("ImagePath",ImagePath);
+                                jsonObject.put("imageStatus",ImageStatus);
+                                System.out.println("ImageStatus "+ImageStatus);
+                                jsonObject.put("imagePath",ImagePath);
                                 jsonArray.put(jsonObject);
-                            }else {
+                            }else{
                                 String ImagePath ="0";
+                                System.out.println(ImagePath);
                                 JSONObject jsonObject = new JSONObject();
                                 jsonObject.put("count",Count);
                                 jsonObject.put("nFPostID",NFPostID);
                                 jsonObject.put("date",Date);
                                 jsonObject.put("time",Time);
                                 jsonObject.put("caption",Caption);
-                                jsonObject.put("ImagePath",ImagePath);
+                                jsonObject.put("imageStatus",ImageStatus);
+                                System.out.println("ImageStatus "+ImageStatus);
+                                jsonObject.put("imagePath",ImagePath);
                                 jsonArray.put(jsonObject);
                             }
                         }
                     }
                 }
-                JSONObject jsonObject = new JSONObject();
-                jsonObject.put("count",Count);
-                jsonObject.put("epostID",EpostID);
-
-                jsonArray.put(jsonObject);
             }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-
+            connection.commit();
+        } catch (SQLException throwables) {
             try{
-
+                if ( connection != null )connection.rollback();
+            }catch ( SQLException E ){
+                E.printStackTrace();
+            }
+            throwables.printStackTrace();
+        }finally {
+            try{
                 if ( connection != null )connection.setAutoCommit( true );
 
                 if ( resultSet != null )resultSet.close();
@@ -109,13 +124,11 @@ public class ReportDisplayDAO {
                 if ( preparedStatement2 != null )preparedStatement2.close();
 
                 if ( connection != null )connection.close();
-
-            }catch ( SQLException E ){
-                E.printStackTrace();
-            }
+            }catch ( SQLException E ){ E.printStackTrace(); }
         }
         return jsonArray;
     }
+
 }
 
 
