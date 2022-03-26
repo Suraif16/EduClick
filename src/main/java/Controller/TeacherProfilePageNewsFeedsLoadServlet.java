a5c5541271f2b38751a6b5680a78e638c08d4e61
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2022-03-26 11:58:46 +0530

    load news feeds in profile page

---
+++ src/main/java/Controller/TeacherProfilePageNewsFeedsLoadServlet.java
@@ -0,0 +1,60 @@
+package Controller;
+
+import DAO.NewsFeedsImageDAO;
+import Model.NewsFeeds;
+import Model.Post;
+import Model.User;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+public class TeacherProfilePageNewsFeedsLoadServlet extends HttpServlet  {
+
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+
+        HttpSession session = request.getSession(false);
+
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("serverResponse" , "Allowed");
+
+        User user = (User) session.getAttribute("User");
+
+        String userId = user.getUserId();
+        String fullName = user.getFullName(userId);
+        System.out.println(fullName);
+
+        System.out.println(userId+"   YYYYYYYYYYYYYYYYY");
+
+        Post post = new Post();
+        JSONArray jsonArray1 = new JSONArray();
+
+        try {
+            jsonArray1 = (JSONArray) post.getProfilePageLoadedNewsFeedsId(userId);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        System.out.println(jsonArray1 +"hhhh");
+
+        jsonObject.put("fullName",fullName);
+        jsonObject.put("jsonArray1",jsonArray1);
+
+
+        out.write( jsonObject.toString() );
+        out.close();
+
+
+    }
+    }
