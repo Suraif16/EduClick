f8b9b7711541305538b22117e3f1bff1a36c9000
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2022-03-26 11:59:32 +0530

    load profile page news feeds

---
+++ src/main/webapp/Resources/JS/TeacherProfilePageNewsfeedsLoad.js
@@ -0,0 +1,232 @@
+document.onreadystatechange = function (){
+
+    if ( document.readyState === 'complete' ){
+
+        LoadNewsFeedsInTeacherProfilePage();
+
+    }
+
+}
+
+
+const LoadNewsFeedsInTeacherProfilePage = function (){
+
+    console.log("eekath hariiii");
+
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+
+        if (this.readyState === 4 && this.status === 200){
+            completeNFLoad( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/TeacherProfilePageNewsFeedsLoadServlet" , true);
+    httpreq.send();
+}
+
+function completeNFLoad(httpreq) {
+
+    let jsonResponse = JSON.parse(httpreq.responseText);
+
+    if (jsonResponse.serverResponse === "null Session" || jsonResponse.serverResponse === "Not Allowed") {
+        window.location.replace("/EduClick_war_exploded/Login.html");
+    } else if (jsonResponse.serverResponse === "Allowed") {
+
+        const name = document.getElementById("profileUserName");
+        name.innerHTML = jsonResponse.fullName;
+      //  let url = '/EduClick_war_exploded/userProfileRedirect?userId=' + jsonLoginResponse.userId;
+
+        for( let i=0; i<jsonResponse.jsonArray1.length;i++) {
+
+            console.log(jsonResponse.jsonArray1[i].Caption);
+
+            if (jsonResponse.jsonArray1[i].path !== "" && jsonResponse.jsonArray1[i].Caption !=="") {
+
+                let innerPreviouseHTML = postContents.innerHTML;
+                postContents.innerHTML = '        <div class="post">    ' +
+                    '              <div class="postContentContainer">' +
+                    '                   <div class="postProfileSection">' +
+                    '                        <a href="TeacherProfile.html" class="postProfile">' +
+                    '                           <div class="postProfileImage">' +
+                    '                               <img class="postProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg"> ' +
+                    '                           </div>' +
+                    '                           <div class="postProfileName" >' + jsonResponse.jsonArray1[i].ownerName +
+                    '</div>' + ' </a>' +
+                    '                           <div class="postTimeAndDate" >' +
+                    jsonResponse.jsonArray1[i].Time + ' | ' +
+                    jsonResponse.jsonArray1[i].Date +
+                    '                            </div>' +
+                    '                    <div class="userOptions">' +
+                    '<input class="userOptionsButton" type="button" value="    " id="educationalPostOPtion' + jsonResponse.jsonArray1[i].postId + '" onclick="showOptionMenu(' + jsonResponse.jsonArray1[i].postId + ',\'educationalPostOPtion\')">' +
+                    '                    </div>' +
+                    '                   </div>' +
+                    '               </div>' +
+                    '               <div class="postContentContainer">' +
+                    '                   <div class="postData">' +
+                    '                       <div class="postMessage">' + jsonResponse.jsonArray1[i].Caption +
+
+                    '                       </div>' +
+                    '                       <div class="postPicture">' +
+                    '                           <div class="postPictureImageContainer">' +
+                    '                              <img class="postPictureImage" src="../Resources/Images/NewsFeedImages/' + jsonResponse.jsonArray1[i].path + '.jpeg">' +
+                    '                           </div>' +
+                    '                       </div>' +
+                    '                     </div>' +
+                    '                  </div>' +
+                    '                   <div class="postContentContainer">' +
+                    '                       <div class="postLikeShareButtons">' +
+                    '                           <div class="likeShareButtons" >' +
+                    '                               <input type="button" value="Like" class="like" onclick="likeNewsFeeds(' + jsonResponse.jsonArray1[i].postId + ')">' +
+                    '                           </div>' +
+                    '                           <div class="countOfLikeShare" >' +
+                    '                               <div class="likeCount">' + jsonResponse.jsonArray1[i].likeCount +
+                    '                                   Likes' +
+                    '                               </div >' +
+                    '                               <div class="emptySpaceLikeShare">' +
+                    '                               </div>' +
+                    '                               <div class="shareCount">' + jsonResponse.jsonArray1[i].shareCount +
+                    '                                    Shares' +
+                    '                               </div>' +
+                    '                              </div>' +
+                    '                           <div class="likeShareButtons" >' +
+                    '                               <input type="button" value="Share" class="share" onclick="shareNewsFeeds(' + jsonResponse.jsonArray1[i].postId + ')">' +
+                    '                           </div>' +
+                    '                        </div>' +
+                    '                       </div>' +
+                    '                   </div>'
+
+
+                postContents.innerHTML += innerPreviouseHTML;
+
+
+            }else if(jsonResponse.jsonArray1[i].path === "" && jsonResponse.jsonArray1[i].Caption !==""){
+
+                let innerPreviouseHTML = postContents.innerHTML;
+                postContents.innerHTML = '        <div class="post">    ' +
+                    '              <div class="postContentContainer">' +
+                    '                   <div class="postProfileSection">' +
+                    '                        <a href="TeacherProfile.html" class="postProfile">' +
+                    '                           <div class="postProfileImage">' +
+                    '                               <img class="postProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg"> ' +
+                    '                           </div>' +
+                    '                           <div class="postProfileName" >' + jsonResponse.jsonArray1[i].ownerName +
+                    '</div>' + ' </a>' +
+                    '                           <div class="postTimeAndDate" >' +
+                    jsonResponse.jsonArray1[i].Time + ' | ' +
+                    jsonResponse.jsonArray1[i].Date +
+                    '                            </div>' +
+                    '                    <div class="userOptions">' +
+                    '<input class="userOptionsButton" type="button" value="    " id="educationalPostOPtion' + jsonResponse.jsonArray1[i].postId + '" onclick="showOptionMenu(' + jsonResponse.jsonArray1[i].postId + ',\'educationalPostOPtion\')">' +
+                    '                    </div>' +
+                    '                   </div>' +
+                    '               </div>' +
+                    '               <div class="postContentContainer">' +
+                    '                   <div class="postData">' +
+                    '                       <div class="postMessage">' + jsonResponse.jsonArray1[i].Caption +
+
+                    '                       </div>' +
+
+                    '                     </div>' +
+                    '                  </div>' +
+                    '                   <div class="postContentContainer">' +
+                    '                       <div class="postLikeShareButtons">' +
+                    '                           <div class="likeShareButtons" >' +
+                    '                               <input type="button" value="Like" class="like" onclick="likeNewsFeeds(' + jsonResponse.jsonArray1[i].postId + ')">' +
+                    '                           </div>' +
+                    '                           <div class="countOfLikeShare" >' +
+                    '                               <div class="likeCount">' + jsonResponse.jsonArray1[i].likeCount +
+                    '                                   Likes' +
+                    '                               </div >' +
+                    '                               <div class="emptySpaceLikeShare">' +
+                    '                               </div>' +
+                    '                               <div class="shareCount">' + jsonResponse.jsonArray1[i].shareCount +
+                    '                                    Shares' +
+                    '                               </div>' +
+                    '                              </div>' +
+                    '                           <div class="likeShareButtons" >' +
+                    '                               <input type="button" value="Share" class="share" onclick="shareNewsFeeds(' + jsonResponse.jsonArray1[i].postId + ')">' +
+                    '                           </div>' +
+                    '                        </div>' +
+                    '                       </div>' +
+                    '                   </div>'
+
+
+                postContents.innerHTML += innerPreviouseHTML;
+
+
+            }else if (jsonResponse.jsonArray1[i].Caption === "" && jsonResponse.jsonArray1[i].path !== "")
+            {
+//**********
+                let innerPreviouseHTML = postContents.innerHTML;
+                postContents.innerHTML = '        <div class="post">    ' +
+                    '              <div class="postContentContainer">' +
+                    '                   <div class="postProfileSection">' +
+                    '                        <a href="TeacherProfile.html" class="postProfile">' +
+                    '                           <div class="postProfileImage">' +
+                    '                               <img class="postProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg"> ' +
+                    '                           </div>' +
+                    '                           <div class="postProfileName" >' + jsonResponse.jsonArray1[i].ownerName +
+                    '</div>' + ' </a>' +
+                    '                           <div class="postTimeAndDate" >' +
+                    jsonResponse.jsonArray1[i].Time + ' | ' +
+                    jsonResponse.jsonArray1[i].Date +
+                    '                            </div>' +
+                    '                    <div class="userOptions">' +
+                    '<input class="userOptionsButton" type="button" value="    " id="educationalPostOPtion' + jsonResponse.jsonArray1[i].postId + '" onclick="showOptionMenu(' + jsonResponse.jsonArray1[i].postId + ',\'educationalPostOPtion\')">' +
+                    '                    </div>' +
+                    '                   </div>' +
+                    '               </div>' +
+                    '               <div class="postContentContainer">' +
+                    '                   <div class="postData">' +
+
+                    '                       <div class="postPicture">' +
+                    '                           <div class="postPictureImageContainer">' +
+                    '                              <img class="postPictureImage" src="../Resources/Images/NewsFeedImages/' + jsonResponse.jsonArray1[i].path + '.jpeg">' +
+                    '                           </div>' +
+                    '                       </div>' +
+                    '                     </div>' +
+                    '                  </div>' +
+                    '                   <div class="postContentContainer">' +
+                    '                       <div class="postLikeShareButtons">' +
+                    '                           <div class="likeShareButtons" >' +
+                    '                               <input type="button" value="Like" class="like" onclick="likeNewsFeeds(' + jsonResponse.jsonArray1[i].postId + ')">' +
+                    '                           </div>' +
+                    '                           <div class="countOfLikeShare" >' +
+                    '                               <div class="likeCount">' + jsonResponse.jsonArray1[i].likeCount +
+                    '                                   Likes' +
+                    '                               </div >' +
+                    '                               <div class="emptySpaceLikeShare">' +
+                    '                               </div>' +
+                    '                               <div class="shareCount">' + jsonResponse.jsonArray1[i].shareCount +
+                    '                                    Shares' +
+                    '                               </div>' +
+                    '                              </div>' +
+                    '                           <div class="likeShareButtons" >' +
+                    '                               <input type="button" value="Share" class="share" onclick="shareNewsFeeds(' + jsonResponse.jsonArray1[i].postId + ')">' +
+                    '                           </div>' +
+                    '                        </div>' +
+                    '                       </div>' +
+                    '                   </div>'
+
+
+                postContents.innerHTML += innerPreviouseHTML;
+
+
+
+
+
+            }
+
+
+        }
+
+
+    }
+
+    else
+    {
+        alert("something went wrong!!!");
+    }
+}
