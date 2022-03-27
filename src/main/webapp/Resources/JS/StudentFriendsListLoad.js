daf58a09e9b4ba90d9bc486466cb6b916ed8232d
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2022-03-27 12:40:49 +0530

    load nf

--- src/main/webapp/Resources/JS/StudentFriendsListLoad.js
+++ src/main/webapp/Resources/JS/StudentFriendsListLoad.js
@@ -26,8 +26,7 @@ const loadFriendsList = function (){
         if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
             window.location.replace("/EduClick_war_exploded/Login.html");
         }else if(jsonLoginResponse.serverResponse === "Allowed") {
-            /* This is where I need work everytime as per the authentication filter*/
-            console.log("Onna mama awa hehehehehe friends");
+            console.log(jsonLoginResponse+"Onna mama awa hehehehehe friends");
 
             let count = jsonLoginResponse.List.length - 1;
             for (i = 0 ; i <= count ; i++){
@@ -54,29 +53,6 @@ const loadFriendsList = function (){
         rightPanel.style.display = "flex";
         rightPanel.style.width = "435%";
         rightPanel.style.marginLeft = "230%";
-
-
-        /*let htmlString = '<div class="rightPanelSingleStudent" style=" flex-basis: 15%;' +
-            '    margin: 1% auto;' +
-            '    display: flex;' +
-            '    flex-direction: column;' +
-            '    width: 100%;' +
-            '    background-color: #4775c4;">' +
-            '                <div>' +
-            '                    <a href="/EduClick_war_exploded/userProfileRedirect?userId='+ userID +'" class="profile" style="display: flex;">' +
-            '                        <div class="classroomStudentProfileName">' + firstName + " " +lastName + '</div>' +
-            '                    </a>' +
-            '                </div>';
-
-        htmlString += '       <div>' +
-            '                    <input style="display:block;" id="follow'+ userID +'" type="button" value="Follow" onclick="followUnfollowTeachers(' + userID +')">' +
-            '                    <input style="display:none;" id="unFollow'+ userID +'" type="button" value="Unfollow" class="studentDisable" onclick="followUnfollowTeachers(' +userID +')">' +
-            '                </div>';
-
-
-        htmlString += '</div>';
-
-        searchContent.innerHTML += htmlString;*/
 
 
         let htmlString = '<div id="rightPanel" style="margin: 2% auto 2% auto;' +
