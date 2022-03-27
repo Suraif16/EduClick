c624e76a2fd7673175a01788b2f0ed32976ea83d
Farzan <farzanroxz123@gmail.com>
2022-03-27 18:05:32 +0530

    completed funtion to get user profile details to the frontend

--- target/EduClick-1.0-SNAPSHOT/Resources/JS/EditProfile.js
+++ target/EduClick-1.0-SNAPSHOT/Resources/JS/EditProfile.js
@@ -9,7 +9,36 @@ const showHideUserProfileEditForm = function (){
     }else{
 
         userProfileEditForm.style.display = "flex";
+        getUserProfileDetails();
 
     }
+
+}
+
+const getUserProfileDetails = function (){
+
+    let httpreq = new XMLHttpRequest();
+
+    httpreq.onreadystatechange = function (){
+
+        if ( this.readyState === 4 && this.status === 200 ){
+
+            let jsonResponse = JSON.parse( this.responseText );
+
+            displayUserProfileDetails( jsonResponse );
+
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/user/???" ,true );
+    httpreq.setRequestHeader("Content-type", "application/x-www-form-urlencoded" );
+    httpreq.send( "userId=" + getUserIdClientSide() );
+
+}
+
+const displayUserProfileDetails = function ( jsonResponse ){
+
+    console.log( jsonResponse );
 
 }
