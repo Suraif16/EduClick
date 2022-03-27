2b31ccb28f035ff2fafedce1abbc0034099093dc
Venushka <venushkachstc@gmail.com>
2022-03-27 16:38:25 +0530

    Added a new servlet to load user details

--- src/main/java/Controller/TeacherProfilePageDetailsLoadServlet.java
+++ src/main/java/Controller/TeacherProfilePageDetailsLoadServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class TeacherProfilePageDetaisLoadServlet extends HttpServlet {
+public class TeacherProfilePageDetailsLoadServlet extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
