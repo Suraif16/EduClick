fb5b4a6ad3ce174c17435cd1987d0e5688bfef3a
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2022-03-27 11:20:43 +0530

    load user name

--- src/main/webapp/Resources/JS/TeacherproJS.js
+++ src/main/webapp/Resources/JS/TeacherproJS.js
@@ -1,7 +1,7 @@
 let rightPanelStatus = false; /*if it is false the list is hidden, if it is true the list it visible*/
 const rightPanel = document.getElementById("rightPanel");
 
-/*document.onreadystatechange = function (){
+document.onreadystatechange = function (){
 
     if ( document.readyState === 'complete' ){
 
@@ -9,7 +9,7 @@ const rightPanel = document.getElementById("rightPanel");
 
     }
 
-}*/
+}
 
 function showRightPanel(){
 
@@ -329,3 +329,40 @@ search.addEventListener( "keyup" , function ( event ){
 
 
 
+const LoadName = function (){
+
+    console.log("aaaa");
+
+    let httpreq = new XMLHttpRequest();
+    httpreq.onreadystatechange = function (){
+
+        if (this.readyState === 4 && this.status === 200){
+            completeLogin( this ); /*This is where we get the response when the request was successfully sent and a successfully response is received */
+        }
+
+    }
+
+    httpreq.open( "POST" , "/EduClick_war_exploded/teacher/teacherProNameLoad" , true);
+    httpreq.setRequestHeader("Content-type", "application/x-www-form-urlencoded" );
+    httpreq.send("userId=" + getUserIdClientSide());
+
+    function completeLogin( httpreq ){
+
+        let jsonLoginResponse = JSON.parse(httpreq.responseText);
+
+        if( jsonLoginResponse.serverResponse === "null Session" || jsonLoginResponse.serverResponse === "Not Allowed"){
+            window.location.replace("/EduClick_war_exploded/Login.html");
+        }else if(jsonLoginResponse.serverResponse === "Allowed") {
+
+            const headerName = document.getElementById("profileUserName");
+            headerName.innerHTML = jsonLoginResponse.FullName;
+            console.log(headerName+"&&&&&&&&&&&&");
+
+        }else{
+            alert("something went wrong!!!");
+        }
+
+    }
+
+
+}
