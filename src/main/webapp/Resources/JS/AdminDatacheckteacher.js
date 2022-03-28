70f64676928964034c0e85dc609b7bb4c132cdbc
Rahuram <rahuram66@gmail.com>
2022-03-28 12:11:07 +0530

    display profile pic in teacher details

--- src/main/webapp/Resources/JS/AdminDatacheckteacher.js
+++ src/main/webapp/Resources/JS/AdminDatacheckteacher.js
@@ -33,63 +33,122 @@ const searchFunction = function ( ) {
         console.log(jsonLoginResponse.searchResult.length);
         if(jsonLoginResponse.searchResult.length>0){
             for ( i = 0 ; i < jsonLoginResponse.searchResult.length ; i++ ){
+                if(jsonResponse.searchResult[i].profilePic === undefined){
+                    let post = '<div class="post">'+
+                        '<div class="postContentContainer">'+
+                        '<div class="postProfileSection">'+
+                        '<a href="#" class="postProfile">'+
+                        '<div class="postProfileImage">'+
+                        '<img class="postProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg">'+
+                        '</div>'+
+                        '<div class="postProfileName">'+jsonLoginResponse.searchResult[i].firstName+" "+jsonLoginResponse.searchResult[i].lastName+
+                        '</div>'+
+                        '</a>'+
+                        '</div>'+
+                        '</div>'+
 
-            let post = '<div class="post">'+
-                '<div class="postContentContainer">'+
-                '<div class="postProfileSection">'+
-                '<a href="#" class="postProfile">'+
-                '<div class="postProfileImage">'+
-                '<img class="postProfileIcon" src="../Resources/Icons/account_circle_white_24dp.svg">'+
-                '</div>'+
-                '<div class="postProfileName">'+jsonLoginResponse.searchResult[i].firstName+" "+jsonLoginResponse.searchResult[i].lastName+
-                '</div>'+
-                '</a>'+
-                '</div>'+
-                '</div>'+
+                        '<div class="postContentContainer">'+
+                        '<div class="postData">'+
+                        '<div class="postMessage">'+
+                        'UserId : '+jsonLoginResponse.searchResult[i].userId+
+                        '</div>'+
 
-                '<div class="postContentContainer">'+
-                '<div class="postData">'+
-                '<div class="postMessage">'+
-                'UserId : '+jsonLoginResponse.searchResult[i].userId+
-                '</div>'+
+                        '<div class="postMessage">'+
+                        'FirstName : '+jsonLoginResponse.searchResult[i].firstName+
+                        '</div>'+
 
-                '<div class="postMessage">'+
-                'FirstName : '+jsonLoginResponse.searchResult[i].firstName+
-                '</div>'+
+                        '<div class="postMessage">'+
+                        'LastName : '+jsonLoginResponse.searchResult[i].lastName+
+                        '</div>'+
 
-                '<div class="postMessage">'+
-                'LastName : '+jsonLoginResponse.searchResult[i].lastName+
-                '</div>'+
+                        '<div class="postMessage">'+
+                        'Gender : '+jsonLoginResponse.searchResult[i].gender+
+                        '</div>'+
 
-                '<div class="postMessage">'+
-                'Gender : '+jsonLoginResponse.searchResult[i].gender+
-                '</div>'+
+                        '<div class="postMessage">'+
+                        'DateofBirth : '+jsonLoginResponse.searchResult[i].dataOfBirth+
+                        '</div>'+
 
-                '<div class="postMessage">'+
-                'DateofBirth : '+jsonLoginResponse.searchResult[i].dataOfBirth+
-                '</div>'+
+                        '<div class="postMessage">'+
+                        'City : '+jsonLoginResponse.searchResult[i].city+
+                        '</div>'+
 
-                '<div class="postMessage">'+
-                'City : '+jsonLoginResponse.searchResult[i].city+
-                '</div>'+
-
-                '<div class="postMessage">'+
-                'Country : '+jsonLoginResponse.searchResult[i].country+
-                '</div>'+
+                        '<div class="postMessage">'+
+                        'Country : '+jsonLoginResponse.searchResult[i].country+
+                        '</div>'+
 
 
-                '<div class="postMessage">'+
-                'RegistrationDate : '+jsonLoginResponse.searchResult[i].dataOfReg+
-                '</div>'+
+                        '<div class="postMessage">'+
+                        'RegistrationDate : '+jsonLoginResponse.searchResult[i].dataOfReg+
+                        '</div>'+
 
-                '<div class="postMessage">'+
-                'RegistrationTime : '+jsonLoginResponse.searchResult[i].timeOfReg+
-                '</div>'+
+                        '<div class="postMessage">'+
+                        'RegistrationTime : '+jsonLoginResponse.searchResult[i].timeOfReg+
+                        '</div>'+
 
-                '</div>'+
-                '</div>'+
-                '</div>';
-            postContents.innerHTML += post;
+                        '</div>'+
+                        '</div>'+
+                        '</div>';
+                    postContents.innerHTML += post;
+                }else{
+                    let post = '<div class="post">'+
+                        '<div class="postContentContainer">'+
+                        '<div class="postProfileSection">'+
+                        '<a href="#" class="postProfile">'+
+                        '<div class="postProfileImage">'+
+                        ' <img class="postProfileIcon" src="../Resources/Images/UserProfileImages/profilePicture' + jsonLoginResponse.searchResult[i].profilePic + '.jpeg">'+
+                        '</div>'+
+                        '<div class="postProfileName">'+jsonLoginResponse.searchResult[i].firstName+" "+jsonLoginResponse.searchResult[i].lastName+
+                        '</div>'+
+                        '</a>'+
+                        '</div>'+
+                        '</div>'+
+
+                        '<div class="postContentContainer">'+
+                        '<div class="postData">'+
+                        '<div class="postMessage">'+
+                        'UserId : '+jsonLoginResponse.searchResult[i].userId+
+                        '</div>'+
+
+                        '<div class="postMessage">'+
+                        'FirstName : '+jsonLoginResponse.searchResult[i].firstName+
+                        '</div>'+
+
+                        '<div class="postMessage">'+
+                        'LastName : '+jsonLoginResponse.searchResult[i].lastName+
+                        '</div>'+
+
+                        '<div class="postMessage">'+
+                        'Gender : '+jsonLoginResponse.searchResult[i].gender+
+                        '</div>'+
+
+                        '<div class="postMessage">'+
+                        'DateofBirth : '+jsonLoginResponse.searchResult[i].dataOfBirth+
+                        '</div>'+
+
+                        '<div class="postMessage">'+
+                        'City : '+jsonLoginResponse.searchResult[i].city+
+                        '</div>'+
+
+                        '<div class="postMessage">'+
+                        'Country : '+jsonLoginResponse.searchResult[i].country+
+                        '</div>'+
+
+
+                        '<div class="postMessage">'+
+                        'RegistrationDate : '+jsonLoginResponse.searchResult[i].dataOfReg+
+                        '</div>'+
+
+                        '<div class="postMessage">'+
+                        'RegistrationTime : '+jsonLoginResponse.searchResult[i].timeOfReg+
+                        '</div>'+
+
+                        '</div>'+
+                        '</div>'+
+                        '</div>';
+                    postContents.innerHTML += post;
+                }
+
             }
         }else{
             postContents.innerHTML = '<div class="postMessage">'+
